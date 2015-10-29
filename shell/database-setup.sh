ed86ed51f510a3ba0f0fc211bb68a12eba8e6875
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2015-10-29 14:18:56 -0700

    Expanded vagrant shell scripts
    Added automatic startup configuration for:
    
     * Single database (dev OR test OR prod)
     * `django` DB user
     * Metadata and Feature Def tables (DEPRECATED)
     * Google Social App Authentication
    
    Also includes an update to use python 2.7.10 rather than using 2.7.6 (the latest available via Ubuntu default apt-get)

--- shell/database-setup.sh
+++ shell/database-setup.sh
@@ -2,33 +2,33 @@ export $(cat /home/vagrant/www/.env | grep -v ^# | xargs) 2> /dev/null
 
 # MySQL Install
 echo "Installing MySQL..."
-sudo debconf-set-selections <<< "mysql-server-5.1 mysql-server/root_password password $MYSQL_PASSWORD"
-sudo debconf-set-selections <<< "mysql-server-5.1 mysql-server/root_password_again password $MYSQL_PASSWORD"
+sudo debconf-set-selections <<< "mysql-server-5.1 mysql-server/root_password password $MYSQL_ROOT_PASSWORD"
+sudo debconf-set-selections <<< "mysql-server-5.1 mysql-server/root_password_again password $MYSQL_ROOT_PASSWORD"
 sudo apt-get -qq -y install mysql-server
 
 echo "Creating Databases..."
-mysql -uroot -p$MYSQL_PASSWORD -e 'CREATE DATABASE dev'
-mysql -uroot -p$MYSQL_PASSWORD -e 'CREATE DATABASE test'
-mysql -uroot -p$MYSQL_PASSWORD -e 'CREATE DATABASE prod'
-mysql -uroot -p$MYSQL_PASSWORD -e 'CREATE DATABASE stage'
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $DATABASE_NAME"
 
-export PYTHONPATH=/home/vagrant/www/lib/
+export PYTHONPATH=/home/vagrant/www/lib/:/home/vagrant/www/
 
 echo "Running Migrations..."
 python /home/vagrant/www/manage.py makemigrations
 python /home/vagrant/www/manage.py migrate
 
 echo "Creating Django User..."
-mysql -uroot -p$MYSQL_PASSWORD -e 'GRANT SELECT, INSERT, UPDATE, DELETE ON dev.* TO "django"@"localhost" IDENTIFIED BY "PASSWORD"'
-mysql -uroot -p$MYSQL_PASSWORD -e 'GRANT SELECT, INSERT, UPDATE, DELETE ON test.* TO "django"@"localhost" IDENTIFIED BY "PASSWORD"'
-mysql -uroot -p$MYSQL_PASSWORD -e 'GRANT SELECT, INSERT, UPDATE, DELETE ON prod.* TO "django"@"localhost" IDENTIFIED BY "PASSWORD"'
-mysql -uroot -p$MYSQL_PASSWORD -e 'GRANT SELECT, INSERT, UPDATE, DELETE ON stage.* TO "django"@"localhost" IDENTIFIED BY "PASSWORD"'
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -e "GRANT SELECT, INSERT, UPDATE, DELETE ON $DATABASE_NAME.* TO \"django\"@\"localhost\" IDENTIFIED BY \"PASSWORD\""
 
 # This is legacy code until these tables can be refactored out of the system
+if [ ! -f /home/vagrant/www/scripts/metadata_featdef_tables.sql ]; then
 echo "Downloading SQL Table File..."
-wget -q https://storage.googleapis.com/blink-uploads/metadata_featdef_tables.sql -o /home/vagrant/www/scripts/metadata_featdef_tables.sql
-mysql -uroot -p$MYSQL_PASSWORD < /home/vagrant/www/scripts/metadata_featdef_tables.sql
+wget -q https://storage.googleapis.com/blink-uploads/metadata_featdef_tables.sql -O /home/vagrant/www/scripts/metadata_featdef_tables.sql
+fi
+echo "Applying SQL Table File... (may take a while)"
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -D$DATABASE_NAME < /home/vagrant/www/scripts/metadata_featdef_tables.sql
 
-echo "Adding Cohorot/Site Data..."
-python /home/vagrant/www/scripts/add_alldata_cohort.py $PROJECT_ID
+echo "Adding Cohort/Site Data..."
 python /home/vagrant/www/scripts/add_site_ids.py
+python /home/vagrant/www/scripts/add_alldata_cohort.py $GCLOUD_PROJECT_ID -o cloudsql
+
+echo "Setting Up Social Application Login..."
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -D$DATABASE_NAME -e "BEGIN; INSERT INTO socialaccount_socialapp (provider, name, client_id, secret) VALUES ('google', 'Google', '$GOOGLE_CLIENT_ID', '$GOOGLE_CLIENT_SECRET'); INSERT INTO socialaccount_socialapp_sites (socialapp_id, site_id) VALUES (1, 2), (1, 3), (1, 4); COMMIT;"
--- shell/install-deps.sh
+++ shell/install-deps.sh
@@ -1,9 +1,18 @@
 export $(cat /home/vagrant/www/.env | grep -v ^# | xargs) 2> /dev/null
 
+# Install and update apt-get info
+echo "Preparing System..."
+apt-get -y install software-properties-common
+export CLOUD_SDK_REPO=cloud-sdk-`lsb_release -c -s`
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+# Add apt-get repository to update python from 2.7.6 (default) to latest 2.7.x
+add-apt-repository -y ppa:fkrull/deadsnakes-python2.7
+curl --silent https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+apt-get update -qq
+
 # Install apt-get dependencies
 echo "Installing Dependencies..."
-apt-get update -qq
-apt-get install -qq -y unzip libffi-dev libssl-dev libmysqlclient-dev python2.7-dev git
+apt-get install -qq -y unzip libffi-dev libssl-dev libmysqlclient-dev python2.7 python2.7-dev git
 echo "Dependencies Installed"
 
 # Install PIP + Dependencies
@@ -17,13 +26,11 @@ echo "Installing Google App Engine..."
 wget -q https://storage.googleapis.com/appengine-sdks/featured/google_appengine_1.9.27.zip -O /home/vagrant/google_appengine.zip
 unzip -nq /home/vagrant/google_appengine.zip -d /home/vagrant
 export PATH=$PATH:/home/vagrant/google_appengine/
+mkdir /home/vagrant/www/lib/endpoints/ 2> /dev/null
+cp /home/vagrant/google_appengine/lib/endpoints-1.0/endpoints/* /home/vagrant/www/lib/endpoints/
 echo "Google App Engine Installed"
 
 # Install Google Cloud SDK
 echo "Installing Google Cloud SDK..."
-export CLOUD_SDK_REPO=cloud-sdk-`lsb_release -c -s`
-echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
-curl --silent https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-apt-get -qq update
 apt-get -qq install google-cloud-sdk
 echo "Google Cloud SDK Installed"
