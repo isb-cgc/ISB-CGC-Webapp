c78aed0f4bf4564a0599e4de453fb0c09f3bd3fe
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2015-11-02 12:16:35 -0800

    Fix leftover settings
    Missed two DB configs. I reverted them because I was having issues and trying to debug but forgot to set them back to configurable.
    
    Should be a part of the code review either way, regardless of if this fixes the build on CI or not.

--- GenespotRE/secret_settings.py
+++ GenespotRE/secret_settings.py
@@ -51,22 +51,22 @@ SETTINGS = {
     },
     'LOCAL_DATABASE': {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '127.0.0.1',
-            'PORT': 3306,
-            'NAME': 'dev',
-            'USER': 'root',
-            'PASSWORD': 'root'
+            'ENGINE': os.environ.get('DATABASE_ENGINE', 'django.db.backends.mysql'),
+            'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
+            'PORT': os.environ.get('DATABASE_PORT', 3306),
+            'NAME': os.environ.get('DATABASE_NAME', 'dev'),
+            'USER': os.environ.get('DATABASE_USER'),
+            'PASSWORD': os.environ.get('DATABASE_PASSWORD')
         }
     },
     'TEST_DATABASE': {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'HOST': '127.0.0.1',
-            'PORT': 3306,
-            'NAME': 'test',
-            'USER': 'root',
-            'PASSWORD': 'root'
+            'ENGINE': os.environ.get('DATABASE_ENGINE', 'django.db.backends.mysql'),
+            'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
+            'PORT': os.environ.get('DATABASE_PORT', 3306),
+            'NAME': os.environ.get('DATABASE_NAME', 'test'),
+            'USER': os.environ.get('DATABASE_USER'),
+            'PASSWORD': os.environ.get('DATABASE_PASSWORD')
         }
     },
 
