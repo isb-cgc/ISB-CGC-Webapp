643f778101dcb12d633616e48367eddb5f88f217
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2015-11-03 10:34:36 -0800

    Fix file change listener

--- shell/vagrant-start-server.sh
+++ shell/vagrant-start-server.sh
@@ -6,5 +6,5 @@ sass --watch /home/vagrant/www/sass/main.sass:/home/vagrant/www/static/css/main.
 echo "Starting server..."
 sudo killall python 2> /dev/null
 touch /home/vagrant/parentDir/output.log
-/home/vagrant/google_appengine/dev_appserver.py --skip_sdk_update_check --host 0.0.0.0 --admin_host 0.0.0.0 /home/vagrant/www > /home/vagrant/parentDir/output.log 2> /home/vagrant/parentDir/error.log &
+/home/vagrant/google_appengine/dev_appserver.py --skip_sdk_update_check --use_mtime_file_watcher=True --host 0.0.0.0 --admin_host 0.0.0.0 /home/vagrant/www > /home/vagrant/parentDir/output.log 2> /home/vagrant/parentDir/error.log &
 echo "Server Started"
