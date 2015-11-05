39e8ccc04d41731ecb4418b0ec1480d9447a5801
Ross Bohner <rossbohner@gmail.com>
2015-11-04 16:55:31 -0800

    initial commit

--- workbooks/models.py
+++ workbooks/models.py
@@ -1,164 +1,164 @@
-import operator
+# import operator
 
-from django.db import models
-from django.contrib.auth.models import User
-from django.db.models import Q
+# from django.db import models
+# from django.contrib.auth.models import User
+# from django.db.models import Q
 
 
-class CohortManager(models.Manager):
-    def search(self, search_terms):
-        terms = [term.strip() for term in search_terms.split()]
-        q_objects = []
-        for term in terms:
-            q_objects.append(Q(name__icontains=term))
+# class CohortManager(models.Manager):
+#     def search(self, search_terms):
+#         terms = [term.strip() for term in search_terms.split()]
+#         q_objects = []
+#         for term in terms:
+#             q_objects.append(Q(name__icontains=term))
 
-        # Start with a bare QuerySet
-        qs = self.get_queryset()
+#         # Start with a bare QuerySet
+#         qs = self.get_queryset()
 
-        # Use operator's or_ to string together all of your Q objects.
-        return qs.filter(reduce(operator.and_, [reduce(operator.or_, q_objects), Q(active=True)]))
+#         # Use operator's or_ to string together all of your Q objects.
+#         return qs.filter(reduce(operator.and_, [reduce(operator.or_, q_objects), Q(active=True)]))
 
-    def get_all_tcga_cohort(self):
-        isb_user = User.objects.get(is_superuser=True, username='isb')
-        all_isb_cohort_ids = Cohort_Perms.objects.filter(user=isb_user, perm=Cohort_Perms.OWNER).values_list('cohort_id', flat=True)
-        return Cohort.objects.filter(name='All TCGA Data', id__in=all_isb_cohort_ids)[0]
+#     def get_all_tcga_cohort(self):
+#         isb_user = User.objects.get(is_superuser=True, username='isb')
+#         all_isb_cohort_ids = Cohort_Perms.objects.filter(user=isb_user, perm=Cohort_Perms.OWNER).values_list('cohort_id', flat=True)
+#         return Cohort.objects.filter(name='All TCGA Data', id__in=all_isb_cohort_ids)[0]
 
-class Cohort(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.TextField(null=True)
-    active = models.BooleanField(default=True)
-    last_date_saved = models.DateTimeField(auto_now_add=True)
-    objects = CohortManager()
+# class Cohort(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     name = models.TextField(null=True)
+#     active = models.BooleanField(default=True)
+#     last_date_saved = models.DateTimeField(auto_now_add=True)
+#     objects = CohortManager()
 
-    def sample_size(self):
-        return len(self.samples_set.all())
+#     def sample_size(self):
+#         return len(self.samples_set.all())
 
-    def patient_size(self):
-        return len(self.patients_set.all())
+#     def patient_size(self):
+#         return len(self.patients_set.all())
 
-    '''
-    Returns the highest level of permission the user has.
-    '''
-    def get_perm(self, request):
-        perm = self.cohort_perms_set.filter(user_id=request.user.id).order_by('perm')
+#     '''
+#     Returns the highest level of permission the user has.
+#     '''
+#     def get_perm(self, request):
+#         perm = self.cohort_perms_set.filter(user_id=request.user.id).order_by('perm')
 
-        if len(perm) >= 1:
-            return perm[0]
-        else:
-            return None
+#         if len(perm) >= 1:
+#             return perm[0]
+#         else:
+#             return None
 
-    def get_owner(self):
-        return self.cohort_perms_set.filter(perm=Cohort_Perms.OWNER)[0].user
+#     def get_owner(self):
+#         return self.cohort_perms_set.filter(perm=Cohort_Perms.OWNER)[0].user
 
-    '''
-    Returns a list of filters used on this cohort and all of its parents that were created using a filters.
+#     '''
+#     Returns a list of filters used on this cohort and all of its parents that were created using a filters.
 
-    Filters are only returned if each of the parents were created using 1+ filters.
-    If a cohort is created using some other method, the chain is broken.
-    '''
-    def get_filters(self):
-        filter_list = []
-        # Iterate through all parents
-        cohort = self
-        while cohort:
-            filter_list.extend(Filters.objects.filter(resulting_cohort=cohort))
-            sources = Source.objects.filter(cohort=cohort)
-            if sources:
-                cohort = sources[0].parent
-            else:
-                cohort = None
+#     Filters are only returned if each of the parents were created using 1+ filters.
+#     If a cohort is created using some other method, the chain is broken.
+#     '''
+#     def get_filters(self):
+#         filter_list = []
+#         # Iterate through all parents
+#         cohort = self
+#         while cohort:
+#             filter_list.extend(Filters.objects.filter(resulting_cohort=cohort))
+#             sources = Source.objects.filter(cohort=cohort)
+#             if sources:
+#                 cohort = sources[0].parent
+#             else:
+#                 cohort = None
 
-        return filter_list
+#         return filter_list
 
-    '''
-    Returns a list of notes from its parents.
-    Will only continue up the chain if there is only one parent and it was created by applying filters.
+#     '''
+#     Returns a list of notes from its parents.
+#     Will only continue up the chain if there is only one parent and it was created by applying filters.
 
-    '''
-    def get_revision_history(self):
-        revision_list = []
-        sources = Source.objects.filter(cohort=self)
+#     '''
+#     def get_revision_history(self):
+#         revision_list = []
+#         sources = Source.objects.filter(cohort=self)
 
-        while sources:
-            # single parent
-            if len(sources) == 1:
-                source = sources[0]
-                if source.type == Source.FILTERS:
-                    revision_list.append('Applied Filters.')
-                elif source.type == Source.CLONE:
-                    revision_list.append('Cloned from %s.' % source.parent.name)
-                elif source.type == Source.PLOT_SEL:
-                    revision_list.append('Selected from plot of %s.' % source.parent.name)
-                sources = Source.objects.filter(cohort=source.parent)
+#         while sources:
+#             # single parent
+#             if len(sources) == 1:
+#                 source = sources[0]
+#                 if source.type == Source.FILTERS:
+#                     revision_list.append('Applied Filters.')
+#                 elif source.type == Source.CLONE:
+#                     revision_list.append('Cloned from %s.' % source.parent.name)
+#                 elif source.type == Source.PLOT_SEL:
+#                     revision_list.append('Selected from plot of %s.' % source.parent.name)
+#                 sources = Source.objects.filter(cohort=source.parent)
 
-            # multiple parents
-            if len(sources) > 1:
-                if sources[0].type == Source.SET_OPS:
-                    revision_list.append(sources[0].notes)
-                if sources[0].type == Source.PLOT_SEL:
-                    ret_str = 'Selected from plot of '
-                    first = True
-                    for source in sources:
-                        if first:
-                            ret_str += source.parent.name
-                            first = False
-                        else:
-                            ret_str += ', ' + source.parent.name
-                    revision_list.append(ret_str)
-                # TODO: Actually traverse the tree, but this will require a most sophisticated way of displaying
-                # Currently only getting parents history, and not grandparents history.
-                sources = []
-        if len(revision_list) == 0:
-            revision_list = ['There is no revision history.']
-        return revision_list
+#             # multiple parents
+#             if len(sources) > 1:
+#                 if sources[0].type == Source.SET_OPS:
+#                     revision_list.append(sources[0].notes)
+#                 if sources[0].type == Source.PLOT_SEL:
+#                     ret_str = 'Selected from plot of '
+#                     first = True
+#                     for source in sources:
+#                         if first:
+#                             ret_str += source.parent.name
+#                             first = False
+#                         else:
+#                             ret_str += ', ' + source.parent.name
+#                     revision_list.append(ret_str)
+#                 # TODO: Actually traverse the tree, but this will require a most sophisticated way of displaying
+#                 # Currently only getting parents history, and not grandparents history.
+#                 sources = []
+#         if len(revision_list) == 0:
+#             revision_list = ['There is no revision history.']
+#         return revision_list
 
-    class Meta:
-        verbose_name_plural = "Saved Cohorts"
+#     class Meta:
+#         verbose_name_plural = "Saved Cohorts"
 
-class Samples(models.Model):
-    cohort = models.ForeignKey(Cohort, null=False, blank=False)
-    sample_id = models.TextField(null=False)
+# class Samples(models.Model):
+#     cohort = models.ForeignKey(Cohort, null=False, blank=False)
+#     sample_id = models.TextField(null=False)
 
-class Patients(models.Model):
-    cohort = models.ForeignKey(Cohort, null=False, blank=False)
-    patient_id = models.TextField(null=False)
+# class Patients(models.Model):
+#     cohort = models.ForeignKey(Cohort, null=False, blank=False)
+#     patient_id = models.TextField(null=False)
 
-class Source(models.Model):
-    FILTERS = 'FILTERS'
-    SET_OPS = 'SET_OPS'
-    PLOT_SEL = 'PLOT_SEL'
-    CLONE = 'CLONE'
-    SOURCE_TYPES = (
-        (FILTERS, 'Filters'),
-        (SET_OPS, 'Set Operations'),
-        (PLOT_SEL, 'Plot Selections'),
-        (CLONE, 'Clone')
-    )
+# class Source(models.Model):
+#     FILTERS = 'FILTERS'
+#     SET_OPS = 'SET_OPS'
+#     PLOT_SEL = 'PLOT_SEL'
+#     CLONE = 'CLONE'
+#     SOURCE_TYPES = (
+#         (FILTERS, 'Filters'),
+#         (SET_OPS, 'Set Operations'),
+#         (PLOT_SEL, 'Plot Selections'),
+#         (CLONE, 'Clone')
+#     )
 
-    parent = models.ForeignKey(Cohort, null=True, blank=True, related_name='source_parent')
-    cohort = models.ForeignKey(Cohort, null=False, blank=False, related_name='source_cohort')
-    type = models.CharField(max_length=10, choices=SOURCE_TYPES)
-    notes = models.CharField(max_length=1024, blank=True)
+#     parent = models.ForeignKey(Cohort, null=True, blank=True, related_name='source_parent')
+#     cohort = models.ForeignKey(Cohort, null=False, blank=False, related_name='source_cohort')
+#     type = models.CharField(max_length=10, choices=SOURCE_TYPES)
+#     notes = models.CharField(max_length=1024, blank=True)
 
-class Cohort_Perms(models.Model):
-    READER = 'READER'
-    OWNER = 'OWNER'
-    PERMISSIONS = (
-        (READER, 'Reader'),
-        (OWNER, 'Owner')
-    )
+# class Cohort_Perms(models.Model):
+#     READER = 'READER'
+#     OWNER = 'OWNER'
+#     PERMISSIONS = (
+#         (READER, 'Reader'),
+#         (OWNER, 'Owner')
+#     )
 
-    cohort = models.ForeignKey(Cohort, null=False, blank=False)
-    user = models.ForeignKey(User, null=False, blank=True)
-    perm = models.CharField(max_length=10, choices=PERMISSIONS, default=READER)
+#     cohort = models.ForeignKey(Cohort, null=False, blank=False)
+#     user = models.ForeignKey(User, null=False, blank=True)
+#     perm = models.CharField(max_length=10, choices=PERMISSIONS, default=READER)
 
-class Filters(models.Model):
-    resulting_cohort = models.ForeignKey(Cohort, null=True, blank=True)
-    name = models.CharField(max_length=256, null=False)
-    value = models.CharField(max_length=512, null=False)
+# class Filters(models.Model):
+#     resulting_cohort = models.ForeignKey(Cohort, null=True, blank=True)
+#     name = models.CharField(max_length=256, null=False)
+#     value = models.CharField(max_length=512, null=False)
 
-class Cohort_Comments(models.Model):
-    cohort = models.ForeignKey(Cohort, blank=False, related_name='cohort_comment')
-    user = models.ForeignKey(User, null=False, blank=False)
-    date_created = models.DateTimeField(auto_now_add=True)
-    content = models.CharField(max_length=1024, null=False)
+# class Cohort_Comments(models.Model):
+#     cohort = models.ForeignKey(Cohort, blank=False, related_name='cohort_comment')
+#     user = models.ForeignKey(User, null=False, blank=False)
+#     date_created = models.DateTimeField(auto_now_add=True)
+#     content = models.CharField(max_length=1024, null=False)
--- workbooks/urls.py
+++ workbooks/urls.py
@@ -1,18 +1,18 @@
-from django.conf.urls import patterns, url
+# from django.conf.urls import patterns, url
 
-import views
+# import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.cohort_detail, name='cohort'),
-    url(r'^(?P<cohort_id>\d+)/$', views.cohort_detail, name='cohort_details'),
-    url(r'^filelist/(?P<cohort_id>\d+)/$', views.cohort_filelist, name='cohort_filelist'),
-    url(r'^save_cohort/', views.save_cohort, name='save_cohort'),
-    url(r'^save_cohort_from_plot/', views.save_cohort_from_plot, name='save_cohort_from_plot'),
-    url(r'^delete_cohort/', views.delete_cohort, name='delete_cohort'),
-    url(r'^clone_cohort/(?P<cohort_id>\d+)/', views.clone_cohort, name='clone_cohort'),
-    url(r'^share_cohort/$', views.share_cohort, name='share_cohorts'),
-    url(r'^share_cohort/(?P<cohort_id>\d+)/', views.share_cohort, name='share_cohort'),
-    url(r'^set_operation/', views.set_operation, name='set_operation'),
-    url(r'^save_cohort_comment/', views.save_comment, name='save_cohort_comment'),
-    url(r'^download_filelist/(?P<cohort_id>\d+)/', views.streaming_csv_view, name='download_filelist')
-)
+# urlpatterns = patterns('',
+#     url(r'^$', views.cohort_detail, name='cohort'),
+#     url(r'^(?P<cohort_id>\d+)/$', views.cohort_detail, name='cohort_details'),
+#     url(r'^filelist/(?P<cohort_id>\d+)/$', views.cohort_filelist, name='cohort_filelist'),
+#     url(r'^save_cohort/', views.save_cohort, name='save_cohort'),
+#     url(r'^save_cohort_from_plot/', views.save_cohort_from_plot, name='save_cohort_from_plot'),
+#     url(r'^delete_cohort/', views.delete_cohort, name='delete_cohort'),
+#     url(r'^clone_cohort/(?P<cohort_id>\d+)/', views.clone_cohort, name='clone_cohort'),
+#     url(r'^share_cohort/$', views.share_cohort, name='share_cohorts'),
+#     url(r'^share_cohort/(?P<cohort_id>\d+)/', views.share_cohort, name='share_cohort'),
+#     url(r'^set_operation/', views.set_operation, name='set_operation'),
+#     url(r'^save_cohort_comment/', views.save_comment, name='save_cohort_comment'),
+#     url(r'^download_filelist/(?P<cohort_id>\d+)/', views.streaming_csv_view, name='download_filelist')
+# )
