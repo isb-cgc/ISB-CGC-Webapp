60b690c1a478919c29837ca6c7872b1c1bc20ef6
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2015-11-05 14:38:57 -0800

    Fix GCloud Authentication

--- shell/gcloud_authenticate.sh
+++ shell/gcloud_authenticate.sh
@@ -5,7 +5,7 @@ echo "{\
   \"client_email\": \"$GAE_CLIENT_EMAIL\",\
   \"client_id\": \"$GAE_CLIENT_ID\",\
   \"type\": \"service_account\"\
-}" | tee gae-key.json
-gcloud auth activate-service-account --key-file gae-key.json
-gcloud config set project "$GAE_PROJECT_ID"
+}" | tee gae-key.json > /dev/null 2> /dev/null
+/home/ubuntu/ISB-CGC-Webapp/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file gae-key.json
+/home/ubuntu/ISB-CGC-Webapp/google-cloud-sdk/bin/gcloud config set project "$GAE_PROJECT_ID"
 rm gae-key.json
