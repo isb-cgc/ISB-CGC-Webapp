b6f268b134093ee1cc42b783c21fd5f9b713ea0b
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2015-11-09 16:49:05 -0800

    Allow MySQL SSL configuration for scripts

--- scripts/add_site_ids.py
+++ scripts/add_site_ids.py
@@ -3,7 +3,10 @@ import MySQLdb
 from GenespotRE import secret_settings
 
 db_settings = secret_settings.get('DATABASE')['default']
-db = MySQLdb.connect(host=db_settings['HOST'], port=db_settings['PORT'], db=db_settings['NAME'], user=db_settings['USER'], passwd=db_settings['PASSWORD'])
+ssl = None
+if db_settings['OPTIONS'] and db_settings['OPTIONS']['ssl']:
+    ssl = db_settings['OPTIONS']['ssl']
+db = MySQLdb.connect(host=db_settings['HOST'], port=db_settings['PORT'], db=db_settings['NAME'], user=db_settings['USER'], passwd=db_settings['PASSWORD'], ssl=ssl)
 
 delete_str = 'DELETE FROM django_site WHERE id in (2, 3, 4);'
 insert_str = 'INSERT INTO django_site (id, domain, name) VALUES (%s, %s, %s), (%s, %s, %s), (%s, %s, %s);'
