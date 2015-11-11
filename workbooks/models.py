7d5561188532c47ee34ae9b5939eff97f9349193
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2015-11-11 15:55:39 -0800

    Workbook Last Viewed Table

--- workbooks/models.py
+++ workbooks/models.py
@@ -14,6 +14,23 @@ class Workbook(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     last_date_saved = models.DateTimeField(auto_now_add=True)
 
+    '''
+    Sets the last viewed time for a cohort
+    '''
+    def mark_viewed (self, request, user=None):
+        if user is None:
+            user = request.user
+
+        last_view = self.workbook_last_view_set.filter(user=user)
+        if last_view is None or len(last_view) is 0:
+            last_view = self.workbook_last_view_set.create(user=user)
+        else:
+            last_view = last_view[0]
+
+        last_view.save(False, True)
+
+        return last_view
+
 class Worksheet(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.TextField(null=False, blank=False)
@@ -33,4 +50,7 @@ class Worksheet_Comment(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     content = models.CharField(max_length=1024, null=False)
 
-
+class Workbook_Last_View(models.Model):
+    workbook = models.ForeignKey(Workbook, blank=False)
+    user = models.ForeignKey(User, null=False, blank=False)
+    last_view = models.DateTimeField(auto_now_add=True, auto_now=True)
