b833be2b20764ebf26d9909e1f294993ecdf888d
Li Li <litopia.li@gmail.com>
2015-11-11 11:20:36 -0800

    added dashboard.js to dashboard page

--- workbooks/views.py
+++ workbooks/views.py
@@ -3,16 +3,16 @@ import json
 import re
 from google.appengine.api import urlfetch
 from django.shortcuts import render
-from mock_data import EGFR_GBM_LGG as FAKE_PLOT_DATA
-from maf_api_mock_data import EGFR_BLCA_BRCA as FAKE_MAF_DATA
-from django.conf import settings
+# from mock_data import EGFR_GBM_LGG as FAKE_PLOT_DATA
+# from maf_api_mock_data import EGFR_BLCA_BRCA as FAKE_MAF_DATA
+# from django.conf import settings
 
 
 def workbook_list(request):
     template = 'workbooks/workbook_list.html'
     return render(request, template, context);
 
-@login_required
+# @login_required
 def workbook_detail(request, workbook_id=0):
     if debug: print >> sys.stderr,'Called '+sys._getframe().f_code.co_name
     template = 'workbooks/workbook_detail.html'
