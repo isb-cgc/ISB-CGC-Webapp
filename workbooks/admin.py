b6ce84cb2beb88ed9ade4dfd6d968ce375ab0c29
Ross Bohner <rossbohner@gmail.com>
2015-11-10 22:18:03 -0800

    added ultra vanilla workbooks

--- workbooks/admin.py
+++ workbooks/admin.py
@@ -1,21 +1,3 @@
-# from search.models import SavedSearch
-# from django.contrib import admin
-# from django.contrib.auth.models import User
+from django.contrib import admin
 
-
-# class SavedSearchAdmin(admin.ModelAdmin):
-#     list_display = (
-#         'id',
-#         'name',
-#         # 'search_url',
-#         # 'datatypes',
-#         'last_date_saved',
-#         # 'user_id_id',
-#         'saved_by'
-#     )
-#
-#     def saved_by(self, obj):
-#         return User.objects.get(pk=obj.user_id).username
-#
-#
-# admin.site.register(SavedSearch, SavedSearchAdmin)
+# Register your models here.
