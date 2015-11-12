2152a4cb2768a7adcfccd0df1d62cdec7fd0d7cc
Li Li <litopia.li@gmail.com>
2015-11-12 13:52:51 -0800

    added autoprefixer and sourcemaps

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,10 +2,15 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sassdoc = require('sassdoc');
 var converter = require('sass-convert');
+var autoprefixer = require('gulp-autoprefixer');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function(){
     gulp.src('blink_sass/style.sass')
         .pipe(sass().on('error', sass.logError))
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer())
+        .pipe(sourcemaps.write())
         .pipe(gulp.dest('static/css/'));
 })
 
