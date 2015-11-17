8cc81154f12ef5cf81399d03add6ead7dca111bc
Li Li <litopia.li@gmail.com>
2015-11-16 17:07:18 -0800

    added basic project upload template

--- static/js/base.js
+++ static/js/base.js
@@ -1,6 +1,39 @@
-+function($){
+require.config({
+    baseUrl: '/static/js/',
+    paths: {
+        jquery: 'libs/jquery-1.11.1.min',
+        bootstrap: 'libs/bootstrap.min',
+        jqueryui: 'libs/jquery-ui.min',
+        session_security: 'session_security',
+        underscore: 'libs/underscore-min',
+        assetscore: 'libs/assets.core',
+        assetsresponsive: 'libs/assets.responsive',
+
+    },
+    shim: {
+        'bootstrap': ['jquery'],
+        'jqueryui': ['jquery'],
+        'session_security': ['jquery'],
+        'assetscore': ['jquery', 'bootstrap', 'jqueryui'],
+        'assetsresponsive': ['jquery', 'bootstrap', 'jqueryui'],
+        'underscore': {exports: '_'}
+    }
+});
+
+require([
+    'jquery',
+    'jqueryui',
+    'bootstrap',
+    'session_security',
+    'underscore',
+    'assetscore',
+    'assetsresponsive'
+], function($, jqueryui, bootstrap, session_security, _) {
     'use strict';
 
+    A11y.Core();
+
+    // Menu toggle
     if($(window).scrollTop() < 10){
         $('#subnav').on('hide.bs.collapse', function () {
             $('#body').toggleClass('menu-open');
@@ -10,4 +43,4 @@
         });
     }
 
-}(jQuery)
+})
