00d7f8af8e64121384d06e075f579885cd3fc7b9
Ross Bohner <rossbohner@gmail.com>
2015-11-17 18:31:33 -0800

    added base functionality of the workbook view

--- workbooks/urls.py
+++ workbooks/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls import patterns, url
 import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.workbook_list, name='workbooks'),
-    url(r'^(?P<workbook_id>\d+)/$', views.workbook_detail, name='workbook_detail')
+    url(r'^$', views.workbook_list, name='workbook_list'),
+    url(r'^samples$', views.workbook_samples, name='workbook_samples'),
+    url(r'^(?P<workbook_id>\d+)/$', views.workbook, name='workbook')
 )
--- workbooks/views.py
+++ workbooks/views.py
@@ -8,13 +8,101 @@ from django.contrib.auth.models import User
 from django.conf import settings
 
 
+@login_required
 def workbook_list(request):
     template = 'workbooks/workbook_list.html'
-    return render(request, template, context);
+    return render(request, template, {});
+
+def workbook_samples(request):
+    template = 'workbooks/workbook_samples.html'
+    return render(request, template, {});
 
 @login_required
-def workbook_detail(request, workbook_id=0):
-    if debug: print >> sys.stderr,'Called '+sys._getframe().f_code.co_name
-    template = 'workbooks/workbook_detail.html'
-    return render(request, template, {})
+def workbook(request, workbook_id=0):
+    template = 'workbooks/workbook.html'
+
+    workbook_data = \
+        {'name': 'Test Workbook',
+         'id' : 1,
+         'description': "This is a test workbook and is not attached to any database structure",
+         'share_count': 2,
+         'owner' : {'name' : 'billy'},
+         'worksheet_list' : [
+             {'name' : 'test worksheet one',
+              'id' : 1,
+              'owner' : {'name' : 'billy'},
+              'description' : 'this is a test worksheet and is not attached to any database structure',
+              'gene_list' : [
+                  {'name' : 'gene one'},
+                  {'name' : 'gene two'},
+                  {'name' : 'gene three'},
+                  {'name' : 'gene four'}],
+              'variable_list' : [
+                  {'name' : 'variable one'},
+                  {'name' : 'variable two'},
+                  {'name' : 'variable three'},
+                  {'name' : 'variable four'}],
+             'cohort_list' : [
+                  {'name' : 'cohort one'},
+                  {'name' : 'cohort two'},
+                  {'name' : 'cohort three'},
+                  {'name' : 'cohort four'}],
+             'comment_list' : [
+                  {'text' : 'this is a test comment 1'},
+                  {'text' : 'this is a test comment 2'},
+                  {'text' : 'this is a test comment 3'},
+                  {'name' : 'this is a test comment 4'}]
+             },
+             {'name' : 'test worksheet two',
+              'id' : 2,
+              'description' : 'this is a test worksheet and is not attached to any database structure',
+              'owner' : {'name' : 'billy'},
+              'gene_list' : [
+                  {'name' : 'gene one'},
+                  {'name' : 'gene two'},
+                  {'name' : 'gene three'},
+                  {'name' : 'gene four'}],
+              'variable_list' : [
+                  {'name' : 'variable one'},
+                  {'name' : 'variable two'},
+                  {'name' : 'variable three'},
+                  {'name' : 'variable four'}],
+             'cohort_list' : [
+                  {'name' : 'cohort one'},
+                  {'name' : 'cohort two'},
+                  {'name' : 'cohort three'},
+                  {'name' : 'cohort four'}],
+             'comment_list' : [
+                  {'text' : 'this is a test comment 1'},
+                  {'text' : 'this is a test comment 2'},
+                  {'text' : 'this is a test comment 3'},
+                  {'name' : 'this is a test comment 4'}]
+             },
+             {'name' : 'test worksheet three',
+              'id' : 3,
+              'description' : 'this is a test worksheet and is not attached to any database structure',
+              'owner' : {'name' : 'billy'},
+              'gene_list' : [
+                  {'name' : 'gene one'},
+                  {'name' : 'gene two'},
+                  {'name' : 'gene three'},
+                  {'name' : 'gene four'}],
+              'variable_list' : [
+                  {'name' : 'variable one'},
+                  {'name' : 'variable two'},
+                  {'name' : 'variable three'},
+                  {'name' : 'variable four'}],
+             'cohort_list' : [
+                  {'name' : 'cohort one'},
+                  {'name' : 'cohort two'},
+                  {'name' : 'cohort three'},
+                  {'name' : 'cohort four'}],
+             'comment_list' : [
+                  {'text' : 'this is a test comment 1'},
+                  {'text' : 'this is a test comment 2'},
+                  {'text' : 'this is a test comment 3'},
+                  {'name' : 'this is a test comment 4'}]
+              }
+         ]};
+    return render(request, template, {'workbook' : workbook_data})
 
