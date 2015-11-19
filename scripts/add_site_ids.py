cc27642f01e8e9e5faa424d30e3bcc2d5b0bcaf9
phyllers <plee@systemsbiology.org>
2015-11-18 18:00:12 -0800

    changed site id to be for mvm

--- scripts/add_site_ids.py
+++ scripts/add_site_ids.py
@@ -30,7 +30,7 @@ delete_str = 'DELETE FROM django_site WHERE id in (2, 3, 4);'
 insert_str = 'INSERT INTO django_site (id, domain, name) VALUES (%s, %s, %s), (%s, %s, %s), (%s, %s, %s);'
 insert_tuple = ('2', 'localhost:8000', 'localhost:8000')
 insert_tuple += ('3', 'localhost:8080', 'localhost:8080')
-insert_tuple += ('4', 'isb-cgc.appspot.com', 'isb-cgc.appspot.com')
+insert_tuple += ('4', 'mvm-dot-isb-cgc.appspot.com', 'mvm-dot-isb-cgc.appspot.com')
 
 cursor = db.cursor()
 cursor.execute(delete_str)
