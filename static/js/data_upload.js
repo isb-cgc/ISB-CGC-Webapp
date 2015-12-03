c86dcb0ac0760d61fad707c2601bec401621b326
Li Li <litopia.li@gmail.com>
2015-12-02 16:14:13 -0800

    toggle on project information section

---
+++ static/js/data_upload.js
@@ -0,0 +1,45 @@
+require.config({
+    baseUrl: '/static/js/',
+    paths: {
+        jquery: 'libs/jquery-1.11.1.min',
+        bootstrap: 'libs/bootstrap.min',
+        jqueryui: 'libs/jquery-ui.min',
+        session_security: 'session_security',
+        underscore: 'libs/underscore-min',
+        assetscore: 'libs/assets.core',
+        assetsresponsive: 'libs/assets.responsive',
+        base: 'base',
+    },
+    shim: {
+        'bootstrap': ['jquery'],
+        'jqueryui': ['jquery'],
+        'session_security': ['jquery'],
+        'assetscore': ['jquery', 'bootstrap', 'jqueryui'],
+        'assetsresponsive': ['jquery', 'bootstrap', 'jqueryui'],
+        'underscore': {exports: '_'},
+    }
+});
+
+require([
+    'jquery',
+    'jqueryui',
+    'bootstrap',
+    'session_security',
+    'underscore',
+    'assetscore',
+    'assetsresponsive',
+    'base',
+], function($, jqueryui, bootstrap, session_security, _) {
+    'use strict';
+
+    // using tabs to toggle project information section
+    $('a[data-target="#project-info"]').on('show.bs.tab', function(e){
+        var target = $(this).data('target')
+
+        $(target).collapse('show');
+    }).on('hide.bs.tab', function(e){
+        var target = $(this).data('target')
+
+        $(target).collapse('hide');
+    })
+})
