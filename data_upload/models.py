ba4e95c2b7aee75858adf14bc13f7d805b851413
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2015-12-02 17:40:25 -0800

    Additional Data Upload Models

--- data_upload/models.py
+++ data_upload/models.py
@@ -1,10 +1,9 @@
-import operator
-
 from django.db import models
+from django.contrib import admin
 from django.contrib.auth.models import User
-from django.db.models import Q
 from google_helpers import cloud_file_storage
 from django.core.files.storage import FileSystemStorage
+from django.core.exceptions import ValidationError
 from django.conf import settings
 
 storage_system = FileSystemStorage()
@@ -14,9 +13,36 @@ if settings.USE_CLOUD_STORAGE is not 'False':
 class UserUpload(models.Model):
     id = models.AutoField(primary_key=True)
     owner = models.ForeignKey(User, null=False, blank=False)
-    status = models.TextField(default="Pending")
+    status = models.CharField(max_length=50,default="Pending")
+    jobURL = models.CharField(max_length=250,null=True)
 
 class UserUploadedFile(models.Model):
     id = models.AutoField(primary_key=True)
     upload = models.ForeignKey(UserUpload)
     file = models.FileField(storage=storage_system)
+
+class FieldDataType(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=120)
+    dbString = models.CharField(max_length=64)
+
+    def __str__(self):
+        return self.name
+
+class ControlledFileField(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=120)
+    type = models.ForeignKey(FieldDataType)
+
+class FileDataType(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    fields = models.ManyToManyField(ControlledFileField)
+
+@admin.register(ControlledFileField)
+class ControlledFileFieldAdmin(admin.ModelAdmin):
+    list_display = ('name','type')
+
+@admin.register(FieldDataType)
+class FieldDataTypeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
