a774e85352c3ca3aa38130ee78c0248bc0b682bd
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2015-12-08 12:23:17 -0800

    Mocked Data Upload Working
    Does not create project or study. That is next step.

--- projects/views.py
+++ projects/views.py
@@ -1,11 +1,17 @@
 from copy import deepcopy
-import json
 import re
 from google.appengine.api import urlfetch
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.conf import settings
+from django.http import JsonResponse
+from django.conf import settings
+from data_upload.models import UserUpload, UserUploadedFile
+from projects.models import Project
+
+import json
+import requests
 
 @login_required
 def project_list(request):
@@ -25,3 +31,7 @@ def project_upload(request):
     template = 'projects/project_upload.html'
     context = {}
     return render(request, template, context)
+
+@login_required
+def upload_files(request):
+    return JsonResponse({'status':'success', 'redirect_url': '/projects/1/'})
