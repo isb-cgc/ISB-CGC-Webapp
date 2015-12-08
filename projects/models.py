ba16a6c4ce8c27a32653bdeef4e253d2abc77643
Andrew Hushbeck <andrew.hushbeck@imgineme.com>
2015-12-08 14:40:48 -0800

    Check in study models
    Otherwise the migration is going to keep complaining. We'll clean things up again later.

--- projects/models.py
+++ projects/models.py
@@ -47,3 +47,32 @@ class Project_Last_View(models.Model):
     project = models.ForeignKey(Project, blank=False)
     user = models.ForeignKey(User, null=False, blank=False)
     last_view = models.DateTimeField(auto_now_add=True, auto_now=True)
+
+class Study(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(null=True)
+    active = models.BooleanField(default=True)
+    last_date_saved = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User)
+
+    '''
+    Sets the last viewed time for a cohort
+    '''
+    def mark_viewed (self, request, user=None):
+        if user is None:
+            user = request.user
+
+        last_view = self.study_last_view_set.filter(user=user)
+        if last_view is None or len(last_view) is 0:
+            last_view = self.study_last_view_set.create(user=user)
+        else:
+            last_view = last_view[0]
+
+        last_view.save(False, True)
+
+        return last_view
+
+class Study_Last_View(models.Model):
+    study = models.ForeignKey(Study, blank=False)
+    user = models.ForeignKey(User, null=False, blank=False)
+    last_view = models.DateTimeField(auto_now_add=True, auto_now=True)
