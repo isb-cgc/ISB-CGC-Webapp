be43a00066c009ef9c762a09d9646d6a6a0f73b1
Kelly Iverson <kiverson@plantain.systemsbiology.net>
2015-12-10 16:19:38 -0800

    commented demo/urls

--- demo/urls.py
+++ demo/urls.py
@@ -24,6 +24,6 @@ admin.autodiscover()
 urlpatterns = patterns(
     '',
     url(r'^$', 'demo.views.index', name='saml_index'),
-    url(r'^attrs/$', 'demo.views.attrs', name='attrs'),
-    url(r'^metadata/$', 'demo.views.metadata', name='metadata'),
+    # url(r'^attrs/$', 'demo.views.attrs', name='attrs'),
+    # url(r'^metadata/$', 'demo.views.metadata', name='metadata'),
 )
