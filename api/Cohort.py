01d26b4cb182cea2c6e1433c1b8c4a399d1a3995
Kelly Iverson <kiverson@plantain.systemsbiology.net>
2015-12-14 14:59:23 -0800

    modified datafilenamekey_list to return 'no files found' if empty list

--- api/Cohort.py
+++ api/Cohort.py
@@ -739,6 +739,9 @@ class Cohort_Endpoints_API(remote.Service):
                         bucket_name = 'gs://360ee3ad-mock-mock-mock-52f9a5e7f99a'
                     datafilenamekeys.append("{}{}".format(bucket_name, file_path))
 
+            if not len(datafilenamekeys):
+                datafilenamekeys.append("No files found.")
+
             return DataFileNameKeyList(datafilenamekeys=datafilenamekeys)
 
         except (IndexError, TypeError), e:
