96d2de0a9f2b808c9a19b38741baca33dd161a67
phyllers <plee@systemsbiology.org>
2015-12-14 11:41:08 -0800

    undo edits that were meant for isb-cgc-uat

--- shell/gcloud_authenticate.sh
+++ shell/gcloud_authenticate.sh
@@ -1,10 +1,10 @@
 touch privatekey.json
 echo "{\
-  \"private_key_id\": \"$GAE_PRIVATE_KEY_ID_UAT\",\
-  \"private_key\": \"$GAE_PRIVATE_KEY_UAT\",\
-  \"client_email\": \"$GAE_CLIENT_EMAIL_UAT\",\
-  \"client_id\": \"$GAE_CLIENT_ID_UAT\",\
+  \"private_key_id\": \"$GAE_PRIVATE_KEY_ID\",\
+  \"private_key\": \"$GAE_PRIVATE_KEY\",\
+  \"client_email\": \"$GAE_CLIENT_EMAIL\",\
+  \"client_id\": \"$GAE_CLIENT_ID\",\
   \"type\": \"service_account\"\
 }" | tee privatekey.json > /dev/null 2> /dev/null
 /home/ubuntu/ISB-CGC-Webapp/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file privatekey.json
-/home/ubuntu/ISB-CGC-Webapp/google-cloud-sdk/bin/gcloud config set project "$GAE_PROJECT_ID_UAT"
+/home/ubuntu/ISB-CGC-Webapp/google-cloud-sdk/bin/gcloud config set project "$GAE_PROJECT_ID"
