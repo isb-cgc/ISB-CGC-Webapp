e327993c8f1796135ba70d7a192cad737ee31f21
kleinone <kleinone@systemsbiology.org>
2015-12-24 17:06:37 -0800

    Implemented is_valid_feature_identifier. Added get_provider_class_from_feature_id. Added comments.

--- bq_data_access/data_access.py
+++ bq_data_access/data_access.py
@@ -44,36 +44,90 @@ class FeatureProviderFactory(object):
         return feature_type
 
     @classmethod
-    def from_feature_id(cls, feature_id):
+    def get_provider_class_from_feature_id(cls, feature_id):
+        """
+        Args:
+            feature_id: Feature identifier
+
+        Returns:
+            Feature data provider class for the datatype defined in the
+            feature identifier.
+
+        Raises:
+            FeatureNotFoundException: If the datatype part of the feature
+            identifier is unknown.
+
+        """
         feature_type = cls.get_feature_type_string(feature_id)
         if feature_type is None:
             logging.debug("FeatureProviderFactory.from_feature_id: invalid feature ID: " + str(feature_id))
             raise FeatureNotFoundException(feature_id)
 
         if feature_type == CLINICAL_FEATURE_TYPE:
-            return ClinicalFeatureProvider(feature_id)
+            return ClinicalFeatureProvider
         elif feature_type == GEXP_FEATURE_TYPE:
-            return GEXPFeatureProvider(feature_id)
+            return GEXPFeatureProvider
         elif feature_type == METH_FEATURE_TYPE:
-            return METHFeatureProvider(feature_id)
+            return METHFeatureProvider
         elif feature_type == CNVR_FEATURE_TYPE:
-            return CNVRFeatureProvider(feature_id)
+            return CNVRFeatureProvider
         elif feature_type == RPPA_FEATURE_TYPE:
-            return RPPAFeatureProvider(feature_id)
+            return RPPAFeatureProvider
         elif feature_type == MIRN_FEATURE_TYPE:
-            return MIRNFeatureProvider(feature_id)
+            return MIRNFeatureProvider
         elif feature_type == GNAB_FEATURE_TYPE:
-            return GNABFeatureProvider(feature_id)
+            return GNABFeatureProvider
+
+    @classmethod
+    def from_feature_id(cls, feature_id):
+        provider_class = cls.get_provider_class_from_feature_id(feature_id)
+        return provider_class(feature_id)
+
+
+def is_valid_feature_identifier(feature_id):
+    """
+    Answers if given internal feature identifier is valid.
+
+    Args:
+        feature_id: Internal feature identifier
+
+    Returns:
+        True if feature id is valid, otherwise False.
+    """
+    is_valid = False
+    try:
+        provider_class = FeatureProviderFactory.get_provider_class_from_feature_id(feature_id)
+        is_valid = provider_class.is_valid_feature_id(feature_id)
+    except FeatureNotFoundException:
+        # FeatureProviderFactory.get_provider_class_from_feature_id raises FeatureNotFoundException
+        # if the feature identifier does not look valid. Nothing needs to be done here,
+        # since is_valid is already False.
+        pass
+    finally:
+        return is_valid
 
 
 def get_feature_vector(feature_id, cohort_id_array):
-    #  ex: feature_id 'CLIN:Disease_Code'
+    """
+    Fetches the data from BigQuery tables for a given feature identifier and
+    one or more stored cohorts. Returns the intersection of the samples defined
+    by the feature identifier and the stored cohort.
+
+    Each returned data point is represented as a dict containing patient, sample and
+    aliquot barcodes, and the value as defined by the feature identifier.
+
+    Args:
+        feature_id: Feature identifier
+        cohort_id_array: Array of cohort identifiers (integers)
+
+    Returns:
+        Data as an array of dicts.
+    """
     provider = FeatureProviderFactory.from_feature_id(feature_id)
     cohort_settings = settings.GET_BQ_COHORT_SETTINGS()
 
     result = provider.get_data(cohort_id_array, cohort_settings.dataset_id, cohort_settings.table_id)
-    # ex: result[0]
-    # {'aliquot_id': None, 'patient_id': u'TCGA-BH-A0B1', 'sample_id': u'TCGA-BH-A0B1-10A', 'value': u'BRCA'}
+
     items = []
     for data_point in result:
         data_item = {key: data_point[key] for key in ['patient_id', 'sample_id', 'aliquot_id']}
