373a0a048645c50805e7bbdc0f82630b6b047906
phyllers <phyllers@gmail.com>
2015-12-29 15:21:16 -0800

    testing deployment from blink pr

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -2,13 +2,13 @@
 #./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/staging-ssl/client-cert.pem" ./client-cert.pem
 #./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/staging-ssl/client-key.pem" ./client-key.pem
 #./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/staging-ssl/server-ca.pem" ./server-ca.pem
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${STAGING_ENV_FILE}" ./.env
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${STAGING_CERT_FILE}" ./client-cert.pem
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${STAGING_KEY_FILE}" ./client-key.pem
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${STAGING_CA_FILE}" ./server-ca.pem
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${STAGING_SECRETS_FILE}" ./client_secrets.json
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${STAGING_PEM_FILE}" ./privatekey.pem
-if [-n "$NIH_AUTH_ON" ]; then
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${DEV_ENV_FILE}" ./.env
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${DEV_CERT_FILE}" ./client-cert.pem
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${DEV_KEY_FILE}" ./client-key.pem
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${DEV_CA_FILE}" ./server-ca.pem
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${DEV_SECRETS_FILE}" ./client_secrets.json
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${DEV_PEM_FILE}" ./privatekey.pem
+if [-n "$DEV_NIH_AUTH_ON" ]; then
   ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/saml/advanced_settings.json" ./saml/advanced_settings.json
   ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/saml/settings.json" ./saml/settings.json
   ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/saml/certs/cert.pem" ./saml/certs/cert.pem
