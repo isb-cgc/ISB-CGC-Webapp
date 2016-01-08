3e311cbd30d865fae19ffe48bf90778b28d2dc2e
phyllers <phyllers@gmail.com>
2016-01-08 14:54:41 -0800

    Revert "runtime"
    This reverts commit dcf48feb8372b474c2d21b7688abe5c3121951cd.

--- tasks/views.py
+++ tasks/views.py
@@ -27,7 +27,7 @@ import pytz
 
 import pysftp
 
-import pexpect  # comment this out when running in gae
+#import pexpect  # comment this out when running in gae
 import datetime
 
 from django.http import HttpResponse
