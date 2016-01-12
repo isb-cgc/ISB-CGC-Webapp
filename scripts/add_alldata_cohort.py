9ca754f8ed5120aa399771d9918eb6b7461840c1
phyllers <plee@systemsbiology.org>
2016-01-12 14:17:27 -0800

    added tcga clause in creation of all tcga cohort

--- scripts/add_alldata_cohort.py
+++ scripts/add_alldata_cohort.py
@@ -153,7 +153,7 @@ def get_superuser_id(conn, superuser_name):
 def get_sample_barcodes(conn):
     logging.info("Getting list of sample barcodes from MySQL")
     cursor = conn.cursor(DictCursor)
-    select_samples_str = 'SELECT distinct SampleBarcode from metadata_samples;'
+    select_samples_str = 'SELECT distinct SampleBarcode from metadata_samples where Project="TCGA";'
     cursor.execute(select_samples_str)
     rows = cursor.fetchall()
     cursor.close()
@@ -161,7 +161,7 @@ def get_sample_barcodes(conn):
 
 def get_patient_barcodes(conn):
     cursor = conn.cursor(DictCursor)
-    select_patients_str = 'SELECT DISTINCT ParticipantBarcode from metadata_samples;'
+    select_patients_str = 'SELECT DISTINCT ParticipantBarcode from metadata_samples where Project="TCGA";'
     cursor.execute(select_patients_str)
     rows = cursor.fetchall()
     cursor.close()
