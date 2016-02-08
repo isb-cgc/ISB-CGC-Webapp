4e8a876edb0789be393226de26f2dad5664d53b8
Kelly Iverson <kiverson@plantain.systemsbiology.net>
2016-02-08 15:14:32 -0800

    fixed bug

--- api/api_helpers.py
+++ api/api_helpers.py
@@ -249,7 +249,7 @@ def build_where_clause(dict, alt_key_map=False):
             big_query_str += ' %s in (' % key
             i = 0
             for val in value:
-                value_tuple = value_tuple + (val.strip(),)
+                value_tuple += (val.strip(),) if type(val) is str else (val,)
                 if i == 0:
                     query_str += '%s'
                     big_query_str += '"' + str(val) + '"'
@@ -282,7 +282,7 @@ def build_where_clause(dict, alt_key_map=False):
                 big_query_str += ' %s=' % key
                 query_str += '%s'
                 big_query_str += '"%s"' % value
-                value_tuple = value_tuple + (value.strip(),)
+                value_tuple += (val.strip(),) if type(val) is str else (val,)
     return {'query_str': query_str, 'value_tuple': value_tuple, 'key_order': key_order, 'big_query_str': big_query_str}
 
 
