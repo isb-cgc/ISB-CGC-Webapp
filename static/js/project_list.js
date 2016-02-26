f7ba620af7e7509a355e8248c30a954c03c7d2ed
phyllers <phyllers@gmail.com>
2016-02-26 09:03:26 -0800

    Fixing #179 BlinkUX/ISB-LSDF

--- static/js/project_list.js
+++ static/js/project_list.js
@@ -43,4 +43,42 @@ require([
             .fadeToggle(300);
 
     })
+
+        // Resets forms in modals on cancel. Suppressed warning when leaving page with dirty forms
+    $('.modal').on('hide.bs.modal', function() {
+        var forms = $(this).find('form');
+        if(forms.length)
+            _.each(forms, function (form) {
+                form.reset();
+            });
+    }).find('form').on('submit', function (e) {
+        e.preventDefault();
+        e.stopPropagation();
+
+        var $this = $(this),
+            fields = $this.serialize();
+
+        $this.find('.btn').addClass('btn-disabled').attr('disabled', true);
+        $.ajax({
+            url: $this.attr('action'),
+            data: fields,
+            method: 'POST'
+        }).then(function () {
+            $this.closest('.modal').modal('hide');
+            if($this.data('redirect')) {
+                window.location = $this.data('redirect');
+            } else {
+                window.location.reload();
+            }
+        }, function () {
+            $this.find('.error-messages').append(
+                $('<p>')
+                    .addClass('alert alert-danger')
+                    .text('There was an error deleting that study. Please reload and try again, or try again later.')
+            );
+        })
+        .always(function () {
+            $this.find('.btn').removeClass('btn-disabled').attr('disabled', false);
+        });
+    });
 });
