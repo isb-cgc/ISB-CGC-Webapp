9f68513588d190cdc74be819ad78f4b0544ab74b
s-paquette <spaquett@systemsbiology.org>
2016-06-08 17:33:02 -0700

    -> Per enh #1297, Revision Hisotry on cohort details page will now list the filters applied for 'Applied Filters' revisions

--- static/js/cohort_details.js
+++ static/js/cohort_details.js
@@ -404,5 +404,10 @@ require([
     $('.clone-cohort-btn').on('click', function() {
         $(this).addClass('disabled');
     })
+
+    $('li.applied-filter').each(function(index,elem){
+        $(this).html($(this).text().replace(/\[/g,"<span>").replace(/\]/g,"</span>"));
+    });
+    $('li.applied-filter span').addClass('space-right-5');
 });
 
