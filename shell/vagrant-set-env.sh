d7fc264824e55bf335cf257b371da0b02ea0bb85
kleinone <kleinone@systemsbiology.org>
2016-06-23 17:44:43 -0700

    Added AppEngine to PYTHONPATH

--- shell/vagrant-set-env.sh
+++ shell/vagrant-set-env.sh
@@ -1,2 +1,2 @@
-echo 'export PYTHONPATH=/home/vagrant/www:/home/vagrant/www/lib' | tee -a /home/vagrant/.bash_profile
+echo 'export PYTHONPATH=/home/vagrant/www:/home/vagrant/google_appengine:/home/vagrant/www/lib' | tee -a /home/vagrant/.bash_profile
 chmod +x /home/vagrant/www/shell/python-su.sh
