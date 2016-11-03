209085b1a599a6125d8b9ee9fc6fd80026ff0522
s-paquette <spaquett@systemsbiology.org>
2016-11-02 18:03:03 -0700

    -> Comments

--- static/js/visualizations/plotFactory.js
+++ static/js/visualizations/plotFactory.js
@@ -339,6 +339,8 @@ define([
     function select_plot(args){//plot_selector, legend_selector, pairwise_element, type, x_attr, y_attr, color_by, cohorts, cohort_override, data){
         var width  = $('.worksheet.active .worksheet-panel-body:first').width(), //TODO should be based on size of screen
             height = 725, //TODO ditto
+            // Top margin: required to keep top-most Y-axis ticks from being cut off on non-scrolled y axes
+            // Bottom margin: takes into account double-wrapped x-axis title and wrapped long-text x-axis labels
             margin = {top: 15, bottom: 150, left: 70, right: 10},
             x_type = '',
             y_type = '';
