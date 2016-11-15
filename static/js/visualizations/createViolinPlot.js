4b687c0949952daf0739953ed06cf44dee7cdc45
s-paquette <spaquett@systemsbiology.org>
2016-11-14 16:01:09 -0800

    -> #1599: Gave the x-axis title more space to wrap, enabled full wrapping of y-axis title

--- static/js/visualizations/createViolinPlot.js
+++ static/js/visualizations/createViolinPlot.js
@@ -212,7 +212,7 @@ function($, d3, d3textwrap, vizhelpers) {
                 .style('fill', 'none')
         },
         createViolinPlot: function(svg, raw_Data, height, violin_width, max_y, min_y, xLabel, yLabel, xAttr, yAttr, colorBy, legend, cohort_set) {
-            var margin = {top: 0, bottom: 100, left: 80, right: 0};
+            var margin = {top: 0, bottom: 120, left: 110, right: 0};
             var domain = [min_y, max_y];
             var range = [height-margin.bottom, 0];
             var view_width = 800;
@@ -416,14 +416,21 @@ function($, d3, d3textwrap, vizhelpers) {
                 .attr('transform', 'translate(' + (view_width/2) + ',' + (height - 10) + ')')
                 .text(xLabel);
             d3.select('.x.label').call(d3textwrap.textwrap().bounds({width: (view_width-margin.left)*0.75, height: 50}));
-            d3.select('.x-label-container').selectAll('foreignObject').attr('style','transform: translate(' + ((view_width/2)-(((view_width-margin.left)*0.75)/2)) + 'px,' + (height - 30) + 'px);');
+            d3.select('.x-label-container').selectAll('foreignObject').attr('style','transform: translate(' + ((view_width/2)-(((view_width-margin.left)*0.75)/2)+margin.left) + 'px,' + (height - 50) + 'px);');
             d3.select('.x-label-container').selectAll('div').attr('class','axis-label');
 
-            svg.append('text')
+            svg.append('g')
+                .attr('class', 'y-label-container')
+                .append('text')
                 .attr('class', 'y label')
                 .attr('text-anchor', 'middle')
                 .attr('transform', 'rotate(-90) translate(' + (-1 * (height/2)) + ',10)')
                 .text(yLabel);
+
+            d3.select('.y.label').call(d3textwrap.textwrap().bounds({height: 80, width: (height-margin.top-margin.bottom)*0.75}));
+            d3.select('.y-label-container').selectAll('foreignObject')
+                .attr('style','transform: rotate(-90deg) translate(' + ((-1 * (height-margin.bottom)/2)-(((height-margin.top-margin.bottom)*0.75))/2) + 'px,15px);');
+            d3.select('.y-label-container').selectAll('div').attr('class','axis-label');
 
 
             $('foreignObject div').each(function(){
