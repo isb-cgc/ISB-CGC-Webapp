a255f5c0efe43892b873cbd98d46d76b0e09f040
s-paquette <spaquett@systemsbiology.org>
2017-01-18 16:08:01 -0800

    -> Broke database refresh out into its own script

---
+++ shell/database-refresh.sh
@@ -0,0 +1,11 @@
+if [ -n "$CI" ]; then
+    export "[ERROR] Never refresh databases on deployments!"
+else
+    export $(cat /home/vagrant/www/.env | grep -v ^# | xargs) 2> /dev/null
+    export MYSQL_ROOT_USER=root
+    echo "Dropping database $DATABASE_NAME in prepatation for reload..."
+    mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "DROP DATABASE IF EXISTS $DATABASE_NAME"
+    mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $DATABASE_NAME"
+    echo "Database $DATABASE_NAME dropped and re-created."
+    ( "/home/vagrant/www/shell/database-setup.sh" )
+fi
