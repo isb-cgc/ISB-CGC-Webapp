3e8945ffbe20037dedda24b10b8fe3cdb913e5b5
s-paquette <spaquett@systemsbiology.org>
2017-02-23 15:20:08 -0800

    -> Moving buckets, changing service accounts

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -6,9 +6,9 @@
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_DEV}/${DEV_PEM_FILE}" ./privatekey.pem
 ./google-cloud-sdk/bin/gsutil rsync -R static/ gs://webapp-dev-static-files/static
 if [ -n "${DEV_NIH_AUTH_ON}" ]; then
-  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/saml/advanced_settings.json" ./saml/advanced_settings.json
-  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/saml/settings.json" ./saml/settings.json
-  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/saml/certs/cert.pem" ./saml/certs/cert.pem
-  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/saml/certs/key.pem" ./saml/certs/key.pem
-  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/NIH_FTP.txt" ./NIH_FTP.txt
+  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_DEV}/saml/advanced_settings.json" ./saml/advanced_settings.json
+  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_DEV}/saml/settings.json" ./saml/settings.json
+  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_DEV}/saml/certs/cert.pem" ./saml/certs/cert.pem
+  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_DEV}/saml/certs/key.pem" ./saml/certs/key.pem
+  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_DEV}/NIH_FTP.txt" ./NIH_FTP.txt
 fi
