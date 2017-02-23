05321dc8134508262aea4a369a2f62506a7e6ed8
s-paquette <spaquett@systemsbiology.org>
2017-02-23 15:23:27 -0800

    -> Syncing gcloud auth methods

--- shell/gcloud_authenticate.sh
+++ shell/gcloud_authenticate.sh
@@ -7,4 +7,5 @@ echo "{\
   \"type\": \"service_account\"\
 }" | tee dev_deployment_key.json > /dev/null 2> /dev/null
 /home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file dev_deployment_key.json
+/home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set account $GAE_CLIENT_EMAIL
 /home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set project "$GAE_PROJECT_ID"
