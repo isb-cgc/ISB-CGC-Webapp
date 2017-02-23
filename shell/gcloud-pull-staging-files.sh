b19367a9f330f62e8211dffbc492f0213bc78e74
s-paquette <spaquett@systemsbiology.org>
2017-02-23 15:17:52 -0800

    -> Moving buckets, changing service accounts

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -1,14 +1,14 @@
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${DEV_ENV_FILE}" ./.env
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${DEV_CERT_FILE}" ./client-cert.pem
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${DEV_KEY_FILE}" ./client-key.pem
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${DEV_CA_FILE}" ./server-ca.pem
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${DEV_SECRETS_FILE}" ./client_secrets.json
-./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/${DEV_PEM_FILE}" ./privatekey.pem
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_DEV}/${DEV_ENV_FILE}" ./.env
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_DEV}/${DEV_CERT_FILE}" ./client-cert.pem
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_DEV}/${DEV_KEY_FILE}" ./client-key.pem
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_DEV}/${DEV_CA_FILE}" ./server-ca.pem
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_DEV}/${DEV_SECRETS_FILE}" ./client_secrets.json
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_DEV}/${DEV_PEM_FILE}" ./privatekey.pem
 ./google-cloud-sdk/bin/gsutil rsync -R static/ gs://webapp-dev-static-files/static
 if [ -n "${DEV_NIH_AUTH_ON}" ]; then
-  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/saml/advanced_settings.json" ./saml/advanced_settings.json
-  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/saml/settings.json" ./saml/settings.json
-  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/saml/certs/cert.pem" ./saml/certs/cert.pem
-  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/saml/certs/key.pem" ./saml/certs/key.pem
-  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/dev-files/NIH_FTP.txt" ./NIH_FTP.txt
+  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/saml/advanced_settings.json" ./saml/advanced_settings.json
+  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/saml/settings.json" ./saml/settings.json
+  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/saml/certs/cert.pem" ./saml/certs/cert.pem
+  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/saml/certs/key.pem" ./saml/certs/key.pem
+  ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET}/NIH_FTP.txt" ./NIH_FTP.txt
 fi
