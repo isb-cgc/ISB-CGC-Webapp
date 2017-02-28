0731dc35ff7ea2be6bad1d01ff23ac398c534be5
s-paquette <spaquett@systemsbiology.org>
2017-02-27 17:55:51 -0800

    -> Cleanup

--- GenespotRE/wsgi.py
+++ GenespotRE/wsgi.py
@@ -1,5 +1,4 @@
 """
-
 Copyright 2015, Institute for Systems Biology
 
 Licensed under the Apache License, Version 2.0 (the "License");
@@ -26,7 +25,6 @@ might make sense to replace the whole Django WSGI application with a custom one
 that later delegates to the Django one. For example, you could introduce WSGI
 middleware here, or combine a Django application with an application of another
 framework.
-
 """
 
 # os.environ.setdefault("DJANGO_SETTINGS_MODULE", "GenespotRE.prod_settings")
@@ -36,29 +34,7 @@ framework.
 # setting points here.
 from django.core.wsgi import get_wsgi_application
 
-import sys, os
-
 forwarded_allow_ips = '*'
 secure_scheme_headers = {'X-APPENGINE-HTTPS': 'on'}
 
 application = get_wsgi_application()
-
-print >> sys.stdout, "__name__ =", __name__
-print >> sys.stdout, "__file__ =", __file__
-print >> sys.stdout, "os.getpid() =", os.getpid()
-print >> sys.stdout, "os.getcwd() =", os.getcwd()
-print >> sys.stdout, "os.curdir =", os.curdir
-print >> sys.stdout, "sys.path =", repr(sys.path)
-print >> sys.stdout, "sys.modules.keys() =", repr(sys.modules.keys())
-print >> sys.stdout, "sys.modules.has_key('mysite') =", sys.modules.has_key('mysite')
-if sys.modules.has_key('mysite'):
-  print >> sys.stdout, "sys.modules['mysite'].__name__ =", sys.modules['mysite'].__name__
-  print >> sys.stdout, "sys.modules['mysite'].__file__ =", sys.modules['mysite'].__file__
-  print >> sys.stdout, "os.environ['DJANGO_SETTINGS_MODULE'] =", os.environ.get('DJANGO_SETTINGS_MODULE', None)
-
-print >> sys.stdout, "[STATUS] Successfully retrieved application in wsgi.py"
-
-# Apply WSGI middleware here.
-# from helloworld.wsgi import HelloWorldApplication
-# application = HelloWorldApplication(application)
-
