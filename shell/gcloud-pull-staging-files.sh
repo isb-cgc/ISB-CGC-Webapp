fb70c028d999be9669df8d83045371e764b93dc9
s-paquette <spaquett@systemsbiology.org>
2017-03-09 11:58:09 -0800

    -> extra space

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -1,5 +1,5 @@
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/uat.env" ./.env
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/client_secrets.json" ./client_secrets.json
- ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/uat_application.key.json" ./privatekey.json
+./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/uat_application.key.json" ./privatekey.json
 
 ./google-cloud-sdk/bin/gsutil rsync -R static/ gs://webapp-uat-static-files/static
