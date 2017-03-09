b60130d88e9a64ce451b0c3766ab6199985aee46
s-paquette <spaquett@systemsbiology.org>
2017-03-08 17:08:30 -0800

    -> UAT will use test's api

--- shell/gcloud_authenticate.sh
+++ shell/gcloud_authenticate.sh
@@ -1,10 +1,10 @@
-touch test_deployment.key.json
+touch uat_deployment.key.json
 echo "{\
   \"private_key_id\": \"$GAE_PRIVATE_KEY_ID_UAT\",\
   \"private_key\": \"$GAE_PRIVATE_KEY_UAT\",\
   \"client_email\": \"$GAE_CLIENT_EMAIL_UAT\",\
   \"client_id\": \"$GAE_CLIENT_ID_UAT\",\
   \"type\": \"service_account\"\
-}" | tee privatekey.json > /dev/null 2> /dev/null
-/home/ubuntu/ISB-CGC-Webapp/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file privatekey.json
+}" | tee uat_deployment.key.json > /dev/null 2> /dev/null
+/home/ubuntu/ISB-CGC-Webapp/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file uat_deployment.key.json
 /home/ubuntu/ISB-CGC-Webapp/google-cloud-sdk/bin/gcloud config set project "$GAE_PROJECT_ID_UAT"
