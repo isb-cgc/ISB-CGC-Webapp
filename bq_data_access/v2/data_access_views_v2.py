42b4a60816af92a10017c5d9354d30b455a43801
kleinone <kleinone@systemsbiology.org>
2017-04-20 17:01:12 -0700

    Moved plot query logic to bq_data_access/v2 module

--- bq_data_access/v2/data_access_views_v2.py
+++ bq_data_access/v2/data_access_views_v2.py
@@ -16,23 +16,18 @@ limitations under the License.
 
 """
 
-import json
 import logging
 import math
 import traceback
 import sys
 
-from cohorts.metadata_helpers import get_sql_connection
 from bq_data_access.v2.pairwise import PairwiseInputVector, Pairwise
 from bq_data_access.v2.cohort_cloudsql import CloudSQLCohortAccess
-from bq_data_access.v2.data_access import FeatureIdQueryDescription
+from bq_data_access.v2.feature_id_utils import FeatureIdQueryDescription
 from bq_data_access.v2.data_access import is_valid_feature_identifier, get_feature_vectors_tcga_only
 from bq_data_access.v2.feature_value_types import ValueType, is_log_transformable
 from bq_data_access.v2.utils import DurationLogged
 from bq_data_access.v2.utils import VectorMergeSupport
-from cohorts.metadata_helpers import fetch_isbcgc_project_set
-from django.http import JsonResponse
-from projects.models import Project
 
 logger = logging.getLogger(__name__)
 
@@ -154,9 +149,11 @@ def get_pairwise_result(feature_array):
 
     return results
 
+
 @DurationLogged('FEATURE', 'VECTOR_MERGE')
 def get_merged_dict_timed(vms):
     return vms.get_merged_dict()
+
 
 # TODO refactor missing value logic out of this module
 @DurationLogged('FEATURE', 'GET_VECTORS')
@@ -335,6 +332,7 @@ def get_merged_feature_vectors(x_id, y_id, c_id, cohort_id_array, logTransform, 
         results['pairwise_result'] = [pairwise_result]
     return results
 
+
 def get_feature_id_validity_for_array(feature_id_array):
     """
     For each feature identifier in an array, check whether or not the identifier is
@@ -352,71 +350,4 @@ def get_feature_id_validity_for_array(feature_id_array):
 
     return result
 
-def data_access_for_plot(request):
-    """ Used by the web application."""
-    try:
-        ver = request.GET.get('ver', '1')
-        x_id = request.GET.get('x_id', None)
-        y_id = request.GET.get('y_id', None)
-        c_id = request.GET.get('c_id', None)
-        logTransform = json.loads(request.GET.get('log_transform', None))
-        cohort_id_array = request.GET.getlist('cohort_id', None)
-
-        # Check that all requested feature identifiers are valid. Do not check for y_id if it is not
-        # supplied in the request.
-        feature_ids_to_check = [x_id]
-        if c_id is not None:
-            feature_ids_to_check.append(c_id)
-        if y_id is not None:
-            feature_ids_to_check.append(y_id)
-
-        valid_features = get_feature_id_validity_for_array(feature_ids_to_check)
-
-        for feature_id, is_valid in valid_features:
-            logging.info((feature_id, is_valid))
-            if not is_valid:
-                logging.error("Invalid internal feature ID '{}'".format(feature_id))
-                raise Exception('Feature Not Found')
-
-        # Get the project IDs these cohorts' samples come from
-        cohort_vals = ()
-        cohort_params = ""
-
-        for cohort in cohort_id_array:
-            cohort_params += "%s,"
-            cohort_vals += (cohort,)
-
-        cohort_params = cohort_params[:-1]
-
-        db = get_sql_connection()
-        cursor = db.cursor()
-
-        tcga_studies = fetch_isbcgc_project_set()
-
-        cursor.execute("SELECT DISTINCT project_id FROM cohorts_samples WHERE cohort_id IN ("+cohort_params+");",cohort_vals)
-
-        # Only samples whose source studies are TCGA studies, or extended from them, should be used
-        confirmed_study_ids = []
-        unconfirmed_study_ids = []
-
-        for row in cursor.fetchall():
-            if row[0] in tcga_studies:
-                if row[0] not in confirmed_study_ids:
-                    confirmed_study_ids.append(row[0])
-            elif row[0] not in unconfirmed_study_ids:
-                unconfirmed_study_ids.append(row[0])
-
-        if len(unconfirmed_study_ids) > 0:
-            projects = Project.objects.filter(id__in=unconfirmed_study_ids)
-
-            for project in projects:
-                if project.get_my_root_and_depth()['root'] in tcga_studies:
-                    confirmed_study_ids.append(project.id)
-
-        return JsonResponse(get_merged_feature_vectors(x_id, y_id, c_id, cohort_id_array, logTransform, confirmed_study_ids))
-
-    except Exception as e:
-        print >> sys.stdout, traceback.format_exc()
-        logger.exception(e)
-        return JsonResponse({'error': str(e)}, status=500)
 
---
+++ visualizations/data_access_views_v2.py
@@ -0,0 +1,100 @@
+"""
+
+Copyright 2017, Institute for Systems Biology
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+import json
+import logging
+import traceback
+import sys
+
+from cohorts.metadata_helpers import get_sql_connection
+from bq_data_access.v2.data_access_views_v2 import get_merged_feature_vectors, get_feature_id_validity_for_array
+from cohorts.metadata_helpers import fetch_isbcgc_project_set
+from django.http import JsonResponse
+from projects.models import Project
+
+logger = logging.getLogger(__name__)
+
+
+def data_access_for_plot(request):
+    """ Used by the web application."""
+    try:
+        ver = request.GET.get('ver', '1')
+        x_id = request.GET.get('x_id', None)
+        y_id = request.GET.get('y_id', None)
+        c_id = request.GET.get('c_id', None)
+        logTransform = json.loads(request.GET.get('log_transform', None))
+        cohort_id_array = request.GET.getlist('cohort_id', None)
+
+        # Check that all requested feature identifiers are valid. Do not check for y_id if it is not
+        # supplied in the request.
+        feature_ids_to_check = [x_id]
+        if c_id is not None:
+            feature_ids_to_check.append(c_id)
+        if y_id is not None:
+            feature_ids_to_check.append(y_id)
+
+        valid_features = get_feature_id_validity_for_array(feature_ids_to_check)
+
+        for feature_id, is_valid in valid_features:
+            logging.info((feature_id, is_valid))
+            if not is_valid:
+                logging.error("Invalid internal feature ID '{}'".format(feature_id))
+                raise Exception('Feature Not Found')
+
+        # Get the project IDs these cohorts' samples come from
+        cohort_vals = ()
+        cohort_params = ""
+
+        for cohort in cohort_id_array:
+            cohort_params += "%s,"
+            cohort_vals += (cohort,)
+
+        cohort_params = cohort_params[:-1]
+
+        db = get_sql_connection()
+        cursor = db.cursor()
+
+        tcga_studies = fetch_isbcgc_project_set()
+
+        cursor.execute("SELECT DISTINCT project_id FROM cohorts_samples WHERE cohort_id IN ("+cohort_params+");",cohort_vals)
+
+        # Only samples whose source studies are TCGA studies, or extended from them, should be used
+        confirmed_study_ids = []
+        unconfirmed_study_ids = []
+
+        for row in cursor.fetchall():
+            if row[0] in tcga_studies:
+                if row[0] not in confirmed_study_ids:
+                    confirmed_study_ids.append(row[0])
+            elif row[0] not in unconfirmed_study_ids:
+                unconfirmed_study_ids.append(row[0])
+
+        if len(unconfirmed_study_ids) > 0:
+            projects = Project.objects.filter(id__in=unconfirmed_study_ids)
+
+            for project in projects:
+                if project.get_my_root_and_depth()['root'] in tcga_studies:
+                    confirmed_study_ids.append(project.id)
+
+        return JsonResponse(get_merged_feature_vectors(x_id, y_id, c_id, cohort_id_array, logTransform, confirmed_study_ids))
+
+    except Exception as e:
+        print >> sys.stdout, traceback.format_exc()
+        logger.exception(e)
+        return JsonResponse({'error': str(e)}, status=500)
+
