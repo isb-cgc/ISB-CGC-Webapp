861d3c479e16f27bd249d0c298d10e30e9625ac4
kleinone <kleinone@systemsbiology.org>
2017-04-25 12:20:37 -0700

    Better command line parameter handling

--- scripts/feature_def_gen/bq_plot_tool.py
+++ scripts/feature_def_gen/bq_plot_tool.py
@@ -22,10 +22,6 @@ from sys import exit as sys_exit
 import click
 
 from bq_data_access.v2.feature_id_utils import FeatureProviderFactory
-from bq_data_access.v2.data_access import FeatureVectorBigQueryBuilder
-from bq_data_access.bigquery_cohorts import BigQueryCohortStorageSettings
-from bq_data_access.v2.plot_data_support import get_merged_feature_vectors
-from google_helpers.bigquery_service_v2 import BigQueryServiceSupport
 
 logging.basicConfig(level=logging.INFO)
 logger = logging
@@ -38,47 +34,83 @@ def get_bq_program_set(program_array):
 
 @click.command()
 # Google Cloud Platform project number
-@click.argument('project_id', type=click.INT)
+@click.argument('gcp_project_id', type=click.INT)
 # Feature identifier for x-axis
 @click.argument('x', type=str)
 # Feature identifier for y-axis
 @click.argument('y', type=str)
-@click.argument('cohort_id', type=str)
-@click.option('--program', '-p', multiple=True)
-@click.option('--cohort-table', type=str, default="test-project:cohort_dataset.cohorts")
+@click.option('-c', '--cohort_id', "cohort_id_array", type=click.INT, multiple=True, help="Cohort ID number")
+@click.option('-p', '--program', "program_array", type=str, multiple=True, help="Program name ('tcga', 'target')")
+@click.option('-j', '--project', "project_id_array", type=click.INT, multiple=True, help="Project ID number")
+@click.option('--cohort-table', "cohort_table_id", type=str, default="test-project:cohort_dataset.cohorts", help="Cohort table identifier, including project and dataset name.")
 @click.option('--log-transform', type=str, help="Log transform JSON object")
-def run_query(project_id, x, y, cohort_id, program, cohort_table, log_transform):
-    program_set = get_bq_program_set(program)
+def run_query(gcp_project_id, x, y, cohort_id_array, program_array, project_id_array, cohort_table_id, log_transform):
+    from bq_data_access.v2.data_access import FeatureVectorBigQueryBuilder
+    from bq_data_access.bigquery_cohorts import BigQueryCohortStorageSettings
+    from bq_data_access.v2.plot_data_support import get_merged_feature_vectors
+    from google_helpers.bigquery_service_v2 import BigQueryServiceSupport
+
+    # Verify the program set
+    # ----------------------
+    program_set = get_bq_program_set(program_array)
     logger.info("Selected programs: {}".format(program_set))
     if len(program_set) == 0:
-        logger.info("No selected programs, quitting.")
+        logger.info("No programs set. Please include at least one program.")
         sys_exit(0)
 
-    cohort_settings = BigQueryCohortStorageSettings.build_from_full_table_id(cohort_table)
-    bqss = BigQueryServiceSupport.build_from_application_default()
-    fvb = FeatureVectorBigQueryBuilder(project_id, cohort_settings, bqss)
+    # Verify the cohort ID array
+    # --------------------------
+    if len(cohort_id_array) == 0:
+        logger.info("No cohort IDs set. Please include at least one cohort ID.")
+        sys_exit(0)
 
-    data = get_merged_feature_vectors(fvb, x, y, None, [cohort_id], None, None, program_set=program_set)
+    # Verify the project ID array
+    # ---------------------------
+    if len(project_id_array) == 0:
+        logger.info("No project IDs set. Please include at least one project ID.")
+        sys_exit(0)
+
+    cohort_settings = BigQueryCohortStorageSettings.build_from_full_table_id(cohort_table_id)
+    bqss = BigQueryServiceSupport.build_from_application_default()
+    fvb = FeatureVectorBigQueryBuilder(gcp_project_id, cohort_settings, bqss)
+
+    data = get_merged_feature_vectors(fvb, x, y, None, cohort_id_array, log_transform, project_id_array, program_set=program_set)
 
 
 @click.command()
 @click.argument('feature_id', type=str)
-@click.argument('cohort_id', type=str)
-@click.option('--program', '-p', multiple=True)
-@click.option('--cohort-table', type=str, default="test-project:cohort_dataset.cohorts")
-def print_query(feature_id, cohort_id, program, cohort_table):
-    program_set = get_bq_program_set(program)
+@click.option('-c', '--cohort_id', "cohort_id_array", type=click.INT, multiple=True, help="Cohort ID number")
+@click.option('-p', '--program', "program_array", type=str, multiple=True, help="Program name ('tcga', 'target')")
+@click.option('-j', '--project', "project_id_array", type=click.INT, multiple=True, help="Project ID number")
+@click.option('--cohort-table', "cohort_table_id", type=str, default="test-project:cohort_dataset.cohorts", help="Cohort table identifier, including project and dataset name.")
+def print_query(feature_id, cohort_id_array, program_array, project_id_array, cohort_table_id):
+    program_set = get_bq_program_set(program_array)
     logger.info("Selected programs: {}".format(program_set))
+
+    # Verify the program set
+    # ----------------------
     if len(program_set) == 0:
-        logger.info("No selected programs, quitting.")
+        logger.info("No programs set. Please include at least one program.")
+        sys_exit(0)
+
+    # Verify the cohort ID array
+    # --------------------------
+    if len(cohort_id_array) == 0:
+        logger.info("No cohort IDs set. Please include at least one cohort ID.")
+        sys_exit(0)
+
+    # Verify the project ID array
+    # ---------------------------
+    if len(project_id_array) == 0:
+        logger.info("No project IDs set. Please include at least one project ID.")
         sys_exit(0)
 
     provider = FeatureProviderFactory.from_feature_id(feature_id)
 
     query_string = provider.build_query(program_set,
-                                        cohort_table,
-                                        [cohort_id],
-                                        None
+                                        cohort_table_id,
+                                        cohort_id_array,
+                                        project_id_array
                                         )
 
     logger.info("QUERY:\n\n{}".format(query_string))
