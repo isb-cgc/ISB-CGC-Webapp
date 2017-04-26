8579ff374d12193447072a452996dccf8a1cd81b
s-paquette <spaquett@systemsbiology.org>
2017-04-26 12:40:09 -0700

    - Minor auth changes

--- shell/gcloud_authenticate.sh
+++ shell/gcloud_authenticate.sh
@@ -10,5 +10,6 @@ echo "{\
 
 echo "[STATUS] Generated JSON for $UAT_CLIENT_EMAIL, attempting to authenticate..."
 
-/home/ubuntu/ISB-CGC-Webapp/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file uat_deployment.key.json
-/home/ubuntu/ISB-CGC-Webapp/google-cloud-sdk/bin/gcloud config set project "$UAT_PROJECT_ID"
+/home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file uat_deployment.key.json
+/home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set account $UAT_CLIENT_EMAIL
+/home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set project "$UAT_PROJECT_ID"
