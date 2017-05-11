6648c691d5e8443390f40759c0324f6f7617ad65
s-paquette <spaquett@systemsbiology.org>
2017-05-11 15:04:09 -0700

    -> Turning back on health checks and increasing time between checks -> User Data doesn't have data type information

--- static/js/helpers/search_helpers.js
+++ static/js/helpers/search_helpers.js
@@ -317,16 +317,20 @@ function($, tree_graph, stack_bar_chart, draw_parsets) {
                 });
             });
 
-            data_counts.map(function(obj){
-                counts_by_name[obj.name] = {
-                    values: {},
-                    total: obj.total
-                }
-                var values = counts_by_name[obj.name].values;
-                obj.values.map(function(val){
-                    values[val.value] = val.count;
+            // User data (program_id === 0) doesn't have data type information at this time
+            if(program_id > 0) {
+                data_counts.map(function(obj){
+                    counts_by_name[obj.name] = {
+                        values: {},
+                        total: obj.total
+                    }
+                    var values = counts_by_name[obj.name].values;
+                    obj.values.map(function(val){
+                        values[val.value] = val.count;
+                    });
                 });
-            });
+            }
+
 
             $('#'+program_id+'-data-filter-panel li.list-group-item div.cohort-feature-select-block').each(function() {
                 var $this = $(this),
