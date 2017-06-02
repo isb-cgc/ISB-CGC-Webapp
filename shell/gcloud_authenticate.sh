8629cd9660f0a18129d343033802e45f4683a502
s-paquette <spaquett@systemsbiology.org>
2017-06-02 14:50:38 -0700

    -> Fix deployment key filename

--- shell/gcloud_authenticate.sh
+++ shell/gcloud_authenticate.sh
@@ -1,4 +1,4 @@
-touch uat_deployment.key.json
+touch deployment.key.json
 
 echo "[STATUS] Attempting to build key for $UAT_CLIENT_EMAIL..."
 
@@ -8,10 +8,10 @@ echo "{\
   \"client_email\": \"$UAT_CLIENT_EMAIL\",\
   \"client_id\": \"$UAT_CLIENT_ID\",\
   \"type\": \"service_account\"\
-}" | tee uat_deployment.key.json > /dev/null 2> /dev/null
+}" | tee deployment.key.json > /dev/null 2> /dev/null
 
 echo "[STATUS] Generated JSON for $UAT_CLIENT_EMAIL, attempting to authenticate..."
 
-/home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file uat_deployment.key.json
+/home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file deployment.key.json
 /home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set account $UAT_CLIENT_EMAIL
 /home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set project "$UAT_PROJECT_ID"
