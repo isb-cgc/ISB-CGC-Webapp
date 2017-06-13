f79af27d583899073ae2b2cce549f99a1dc9c753
kleinone <kleinone@systemsbiology.org>
2017-06-13 15:51:08 -0700

    Added hg38 table to GNAB data configuration. Implemented schema validation for GNAB data configuration.

--- bq_data_access/data_types/gnab.py
+++ bq_data_access/data_types/gnab.py
@@ -1,25 +1,19 @@
 BIGQUERY_CONFIG = {
-    "supported_genomic_builds": ['hg19'],
+    "supported_genomic_builds": ['hg19', 'hg38'],
     "tables": [
         {
             "genomic_build": "hg19",
             "table_id": "isb-cgc:TCGA_hg19_data_v0.Somatic_Mutation_MC3",
             "gene_label_field": "Hugo_Symbol",
             "internal_table_id": "tcga_hg19_mc3",
-            "program": "tcga",
-            "case_barcode_field": "case_barcode",
-            "sample_barcode_tumor_field": "sample_barcode_tumor",
-            "aliquot_barcode_tumor_field": "aliquot_barcode_tumor"
+            "program": "tcga"
         },
         {
             "genomic_build": "hg38",
             "table_id": "isb-cgc:TCGA_hg38_data_v0.Somatic_Mutation",
             "gene_label_field": "Hugo_Symbol",
             "internal_table_id": "tcga_hg38",
-            "program": "tcga",
-            "case_barcode_field": "Matched_Norm_Sample_Barcode",
-            "sample_barcode_tumor_field": "Matched_Norm_Sample_Barcode",
-            "aliquot_barcode_tumor_field": "Tumor_Sample_Barcode"
+            "program": "tcga"
         }
     ]
 }
