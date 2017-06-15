0da38f1be8fcc308e16669f9903d0a6a992d3d65
s-paquette <spaquett@systemsbiology.org>
2017-06-15 13:55:37 -0700

    - Final round of sharing cleanup

--- static/js/cohort_details.js
+++ static/js/cohort_details.js
@@ -744,40 +744,9 @@ require([
         }
     };
 
-    // onClick: Shared With button 
-    $('#shared-with-btn').on('click', function(e){
-        var target = $(this).data('target');
-        $(target + ' a[data-target="#shared-pane"]').tab('show');
-    });
-
     $('#create-cohort-modal form').on('submit', function() {
         save_changes_btn.prop('disabled', 'disabled');
         save_changes_btn_modal.prop('disabled', 'disabled');
-    });
-
-    // onClick: Remove shared user
-    $('.remove-shared-user').on('click', function() {
-        var user_id = $(this).attr('data-user-id');
-        var url = BASE_URL + '/cohorts/unshare_cohort/' + cohort_id + '/';
-        var csrftoken = $.getCookie('csrftoken');
-        var button = $(this);
-        $.ajax({
-            type        :'POST',
-            url         : url,
-            dataType    :'json',
-            data        : {user_id: user_id},
-            beforeSend  : function(xhr){xhr.setRequestHeader("X-CSRFToken", csrftoken);},
-            success : function (data) {
-                button.parents('tr').remove();
-                var count = parseInt($($('.share-count')[0]).html());
-                $('.share-count').each(function() {
-                   $(this).html(count-1);
-                });
-            },
-            error: function () {
-                console.log('Failed to remove user');
-            }
-        })
     });
 
     // Disable the comment button if there's no content in the comment
