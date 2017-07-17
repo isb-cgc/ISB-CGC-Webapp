7e9185641a3ee251c5b9b5f1de5ae4b4ef471aaf
s-paquette <spaquett@systemsbiology.org>
2017-07-17 13:40:43 -0700

    -> Service Account auth dataset column will say 'None' if none are authorized, otherwise it will list the names

--- GenespotRE/templatetags/custom_tags.py
+++ GenespotRE/templatetags/custom_tags.py
@@ -29,6 +29,7 @@ from projects.models import Program
 from workbooks.models import Workbook
 
 
+
 # If an attribute's values should be alphanumerically sorted, list them here
 ALPHANUM_SORT = [
 
@@ -316,6 +317,8 @@ def get_prog_attr(prog, prog_attr):
 
 @register.filter
 def get_values_list(object_list, value):
+    if not object_list:
+        return []
     return object_list.values_list(value, flat=True)
 
 
