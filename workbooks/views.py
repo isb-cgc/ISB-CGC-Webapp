0842461924ec4044aeba2299c3aa04559e9e98f4
s-paquette <spaquett@systemsbiology.org>
2017-08-17 17:51:30 -0700

    -> On the cohort list page, you can only see how many users a cohort is shared with if you are an owner -> On the workbook detail page, you cannot share a workbook if it contains components (cohort, variable list, gene list) which you do not own

--- workbooks/views.py
+++ workbooks/views.py
@@ -1,3 +1,20 @@
+"""
+Copyright 2017, Institute for Systems Biology
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+
 from copy import deepcopy
 import json
 import re
@@ -239,7 +256,7 @@ def workbook(request, workbook_id=0):
 
             return redirect(redirect_url)
 
-        elif request.method == "GET" :
+        elif request.method == "GET":
             if workbook_id:
                 try :
                     ownedWorkbooks = request.user.workbook_set.all().filter(active=True)
