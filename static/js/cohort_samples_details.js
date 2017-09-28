495650a724f3f352f3277d62f32af82ed4feb2d5
s-paquette <spaquett@systemsbiology.org>
2017-09-27 20:39:47 -0700

    -> Further work on #1652

--- static/js/cohort_samples_details.js
+++ static/js/cohort_samples_details.js
@@ -60,13 +60,16 @@ require([
         return cookieValue;
     }
 
-    var validateEntries = function(barcodes) {
+    function validateEntries(barcodes) {
         var result = {
             valid_entries: null,
             invalid_entries: null
         };
 
         barcodes.filter(function(barcode){ return barcode !== ""; }).map(function(barcode) {
+            // Trim leading and trailing quotes
+            barcode = barcode.replace(/(^\s*['"])|(['"]\s*$)/g,"").trim();
+            // Split the entry into its values
             var entry_split = barcode.split(/["']*\s*,\s*["']*/);
             if (entry_split.length !== 3) {
                 if (!result.invalid_entries) {
@@ -81,8 +84,7 @@ require([
             }
         });
 
-        // Any entries which were valid on the split must now be checked against the database
-
+        // Any entries which were valid during the initial parse must now be checked against the database
         if(result.valid_entries) {
             var deferred = $.Deferred();
             var csrftoken = get_cookie('csrftoken');
@@ -101,8 +103,8 @@ require([
                             result.invalid_entries.concat(data.invalid_entries);
                         }
                     }
-                    result.invalid_entries && deferred.reject(result);
-                    !result.invalid_entries && deferred.resolve(result);
+                    result.valid_entries && deferred.resolve(result);
+                    !result.valid_entries && deferred.reject(result);
                 },
                 error: function () {
                     alert("There was an error while parsing your barcode set.")
@@ -112,12 +114,11 @@ require([
         } else {
             return $.Deferred().reject(result);
         }
-    };
+    }
 
 
     // File Upload
     var xhr2 = !! ( window.FormData && ("upload" in ($.ajaxSettings.xhr())  ));
-    var uploaded_list;
     if(!xhr2 || !window.File || !window.FileReader || !window.FileList || !window.Blob){
         var errorMsg = 'Your browser doesn\'t support file upload. Please use the \'Entry\' method instead.';
         $('#file-upload-btn').attr('disabled', true).parent()
@@ -139,7 +140,8 @@ require([
             // check file format against valid file types
             if(validFileTypes.indexOf(ext) < 0){
                 // If file type is not correct
-                alert('Please choose a .txt, .tsv, or .csv file');
+                base.showJsMessage("error","Please choose a .txt, .tsv, or .csv file.",true);
+                fileUploadField.val("");
                 return false;
             } else{
                 $('#selected-file-name').text(file.name);
@@ -154,9 +156,13 @@ require([
                     // Do a rough file content validation
                     var tsvMatch = fr.result.match(/\t/g);
                     var csvMatch = fr.result.match(/,/g);
+                    var whitelistMatch = fr.result.match(base.barcode_file_whitelist);
 
-                    if((tsvMatch && csvMatch) || (!tsvMatch && !csvMatch) || (csvMatch && csvMatch.length % 2 != 0) || (tsvMatch && tsvMatch.length % 2 != 0)) {
-                        alert("This file is not in a valid format. Please supply a comma- or tab-delimited file, in .tsv, .csv, or .txt format.");
+                    if((tsvMatch && csvMatch) || (!tsvMatch && !csvMatch) || (csvMatch && csvMatch.length % 2 != 0) || (tsvMatch && tsvMatch.length % 2 != 0) || whitelistMatch) {
+                        base.showJsMessage("error","This file is not in a valid format. Please supply a comma- or tab-delimited file, in .tsv, .csv, or .txt format.",true);
+                        $('#uploading').removeClass('in');
+                        $('#file-upload-btn').show();
+                        fileUploadField.val("");
                         return false;
                     }
 
@@ -165,21 +171,59 @@ require([
                     // Validate the entries
                     validateEntries(entries).then(
                         function(result){
-
+                            showEntries(result);
                             $('.btn.save').removeAttr('disabled');
                         },function(result){
-
-                            // show error
-
+                            // We only reach this point if no entries are valid, so show an error message as well.
+                            base.showJsMessage("error","None of the supplied barcode entries were valid. Please double-check the format of your entries.",true);
+                            showEntries(result.invalid_entries);
+                            fileUploadField.val("");
                             $('.btn.save').attr('disabled','disabled');
                         }
                     );
                     $('#uploading').removeClass('in');
                     $('#file-upload-btn').show();
+                    fileUploadField.val("");
                 }
                 fr.readAsText(event.target.files.item(0));
+                fileUploadField.val("");
             }
         })
+    }
+
+    function showEntries(result) {
+        if(result.invalid_entries) {
+            var entry_set = "";
+            for(var i=0; i < result.invalid_entries.length; i++) {
+                entry_set += result.invalid_entries[i]+'\n';
+            }
+            $('.invalid-entries').html(entry_set);
+            $('.invalid-entries').show();
+            $('#invalid-not-saved').show();
+        } else {
+            $('.invalid-entries').hide();
+            $('#invalid-not-saved').hide();
+        }
+
+        if(result.valid_entries) {
+            var entry_set = "";
+            for(var i=0; i < result.valid_entries.length; i++) {
+                entry_set += result.valid_entries[i]+'\n';
+            }
+            $('.valid-entries').html(entry_set);
+            $('.valid-entries').show();
+            $('.cohort-counts').empty();
+            for(program in result.counts) {
+                if(result.counts.hasOwnProperty(program)) {
+                    var prod_deets = result.counts[program];
+                    $('.cohort-counts').append('<tr><td>'+program+'</td><td>'+prod_deets['samples']+'</td><td>'+prod_deets['cases']+'</td>');
+                }
+            }
+            $('.cohort-counts').show();
+        } else {
+            $('.valid-entries').hide();
+            $('.cohort-counts').hide();
+        }
     }
 
 
