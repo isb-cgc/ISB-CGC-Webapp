e786384bc528d07923d680a090bdb979ef842f97
s-paquette <spaquett@systemsbiology.org>
2018-03-16 22:43:47 -0700

    -> Split the export modal out into its own template and JS

--- static/js/cohort_filelist.js
+++ static/js/cohort_filelist.js
@@ -25,7 +25,8 @@ require.config({
         session_security: 'session_security',
         underscore: 'libs/underscore-min',
         tokenfield: 'libs/bootstrap-tokenfield.min',
-        base: 'base'
+        base: 'base',
+        bq_export: 'export_to_bq'
     },
     shim: {
         'bootstrap': ['jquery'],
@@ -42,7 +43,8 @@ require([
     'jqueryui',
     'bootstrap',
     'session_security',
-    'tokenfield'
+    'tokenfield',
+    'bq_export'
 ], function ($, base) {
 
     // For manaaging filter changes
@@ -714,159 +716,6 @@ require([
         $(this).parent().parent().siblings('.checkbox').find('input').prop('checked',false);
         update_filters($($(this).parent().parent().siblings('.checkbox').find('input')[0]));
         update_displays($('ul.nav-tabs-files li.active a').data('file-type'));
-    });
-
-    // Filelist Manifest Export to BQ
-    $('.table-type').on('change',function(){
-        $('#export-manifest-table').val('');
-        if($(this).find(':checked').val()=='append') {
-            $('#export-manifest-form input[type="submit"]').attr('disabled','disabled');
-            $('#export-manifest-table option:not([type="label"])').remove();
-            var tables = $('#export-manifest-project-dataset :selected').data('tables');
-            for(var i=0;i<tables.length;i++) {
-                $('#export-manifest-table').append('<option value="'+tables[i]+'">'+tables[i]+'</option>')
-            }
-            $('.table-list').show();
-            $('.new-table-name').hide();
-        } else {
-            $('#export-manifest-form input[type="submit"]').removeAttr('disabled');
-            $('.table-list').hide();
-            $('.new-table-name').show();
-        }
-    });
-
-    $('#new-table-name').on('keypress keyup paste',function (e) {
-        var self = $(this);
-        setTimeout(function() {
-            $('.message-container').empty();
-            var str = self.val();
-
-            if(str.match(/[^A-Za-z0-9_]/)) {
-                e.preventDefault();
-                base.showJsMessage("error", "BigQuery table names are restricted to numbers, letters, and underscores.",false, $('.message-container'));
-                return false;
-            }
-
-            if (str.length >= parseInt($('#new-table-name').attr('maxlength'))) {
-                e.preventDefault();
-                base.showJsMessage("warning", "You have reached the maximum size of the table name.",false, $('.message-container'));
-                return false;
-            }
-        },70);
-    });
-
-    $('#export-manifest-table').on('change',function(){
-        if($(this).find(':selected').attr('type') !== "label") {
-            $('#export-manifest-form input[type="submit"]').removeAttr('disabled');
-        }
-    });
-
-    $('#export-manifest-project-dataset').on('change',function(){
-        $('.table-type, .new-table-name').removeAttr('disabled');
-        $('.table-type, .new-table-name').removeAttr('title');
-        $('#export-manifest-table option:not([type="label"])').remove();
-        if($('.table-type').find(':checked').val() == 'append') {
-            if($('#export-manifest-table :selected').attr('type') !== "label") {
-                $('#export-manifest-form input[type="submit"]').removeAttr('disabled');
-            } else {
-                $('#export-manifest-form input[type="submit"]').attr('disabled','disabled');
-            }
-        } else {
-            $('#export-manifest-form input[type="submit"]').removeAttr('disabled');
-        }
-
-        var tables = $('#export-manifest-project-dataset :selected').data('tables');
-        if(tables.length > 0) {
-            $('input.table-type[value="append"]').removeAttr('disabled');
-            $('input.table-type[value="append"]').parents('label').removeAttr('title');
-            for (var i = 0; i < tables.length; i++) {
-                $('#export-manifest-table').append('<option value="' + tables[i] + '">' + tables[i] + '</option>')
-            }
-        } else {
-            $('input.table-type[value="append"]').attr('disabled','disabled');
-            $('input.table-type[value="append"]').parents('label').attr('title',"There are no tables in this dataset.");
-        }
-    });
-
-    $('.data-tab-content').on('click','#export-manifest-modal input[type="submit"]',function(){
-        $('#exporting-cohort').css('display','inline-block');
-    });
-
-    $('.data-tab-content').on('hide.bs.modal', '#export-manifest-modal', function(){
-        $('#export-manifest-project-dataset optgroup').remove();
-        $('.table-type, .new-table-name').attr('disabled','disabled');
-        $('.table-type, .new-table-name').attr('title','Select a project and dataset to enable this option');
-        $('.new-table-name').show();
-        $('.table-list').hide();
-        $('.message-container').empty();
-        $('#export-manifest-table option:not([type="label"])').remove();
-        $('#export-manifest-form input[type="submit"]').attr('disabled','disabled');
-    });
-
-    $('.data-tab-content').on('click', 'button[data-target="#export-manifest-modal"]', function(e){
-        if($('#export-manifest-modal').data('opening')) {
-            e.preventDefault();
-            return false;
-        }
-        $('#export-manifest-modal').data('opening',true);
-        $('#export-manifest-form input[type="submit"]').attr('disabled','disabled');
-        $.ajax({
-            type: 'GET',
-            url: BASE_URL + '/cohorts/filelist/export',
-            success: function (data) {
-                if(data['status']==='success') {
-                    if(Object.keys(data['data']['projects']).length > 0) {
-                        var projects = data['data']['projects'];
-                        for(var i=0;i<projects.length;i++) {
-                            if($('optgroup.'+projects[i]['name']).length <= 0) {
-                                $('#export-manifest-project-dataset').append('<optgroup class="'+projects[i]['name']+'" label="'+projects[i]['name']+'"></optgroup>');
-                            }
-                            var datasets = projects[i]['datasets'];
-                            for(var j in datasets) {
-                                if(datasets.hasOwnProperty(j)) {
-                                    $('optgroup.'+projects[i]['name']).append(
-                                        '<option class="dataset" value="'+projects[i]['name']+':'+j+'">'+j+'</option>'
-                                    );
-                                    $('option[value="'+projects[i]['name']+':'+j+'"]').data({'tables': datasets[j]});
-                                }
-                            }
-                        }
-                    }
-                }
-            },
-            error: function (data) {
-                var link_to_bqr = data.responseJSON.msg.match(/register at least one dataset/);
-                var link_to_gcpr = data.responseJSON.msg.match(/register at least one project/);
-                if(link_to_bqr) {
-                    data.responseJSON.msg = data.responseJSON.msg.replace(
-                        "register at least one dataset",
-                        '<a href="http://isb-cancer-genomics-cloud.readthedocs.io/en/latest/sections/webapp/program_data_upload.html#registering-cloud-storage-buckets-and-bigquery-datasets-a-pre-requisite-for-using-your-own-data-in-isb-cgc" target="_BLANK">register at least one dataset</a>'
-                    );
-                }
-                if(link_to_gcpr) {
-                    data.responseJSON.msg = data.responseJSON.msg.replace(
-                        "register at least one project",
-                        '<a href="http://isb-cancer-genomics-cloud.readthedocs.io/en/latest/sections/webapp/Gaining-Access-To-Contolled-Access-Data.html?#registering-your-google-cloud-project-service-account" target="_BLANK">register at least one project</a>'
-                    );
-                }
-                base.showJsMessage('error',data.responseJSON.msg,true,"#export-manifest-js-messages");
-            },
-            complete: function() {
-                $('#export-manifest-modal').modal('show');
-                $('#export-manifest-modal').data('opening',false);
-            }
-        });
-        // Don't let the modal open automatically; we're controlling that.
-        e.preventDefault();
-        return false;
-    });
-
-
-    $('#export-manifest-form').on('submit',function(){
-        if($('.table-type :checked').val() == 'new') {
-            $('#export-manifest-table').val('');
-        }
-        $('#export-manifest-form input[type="submit"]').attr('disabled','disabled');
     });
 
     browser_tab_load(cohort_id);
