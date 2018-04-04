68e836d967101ee1ad6638c43a2c0da63f6af27c
s-paquette <spaquett@systemsbiology.org>
2018-04-04 16:17:35 -0700

    -> For big file list export fix: send total expected file count

--- static/js/cohort_filelist.js
+++ static/js/cohort_filelist.js
@@ -349,6 +349,7 @@ require([
         // Clear the previous parameter settings from the export form, and re-add the build
         $('#export-to-bq-form input.param').remove();
         $('#export-to-bq-form').append('<input class="param" type="hidden" name="build" value="'+build+'" />');
+        $('#export-to-bq-form').append('<input class="param" type="hidden" name="total_expected" value="'+file_list_total+'" />');
 
         var downloadToken = new Date().getTime();
         $('.filelist-obtain .download-token').val(downloadToken);
