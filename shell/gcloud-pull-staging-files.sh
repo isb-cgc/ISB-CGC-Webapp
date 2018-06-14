13dd80863bec2a7b47e2b0797a50ea1ae3b0b824
s-paquette <spaquett@systemsbiology.org>
2018-06-14 13:30:50 -0700

    -> Add in pack/unpack process for staged JSON and txt files

--- shell/gcloud-pull-staging-files.sh
+++ shell/gcloud-pull-staging-files.sh
@@ -1,3 +1,6 @@
+mkdir ./json
+mkdir ./txt
+
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/${UAT_ENV_FILE}" ./.env
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/${UAT_SECRETS_FILE}" ./client_secrets.json
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/${UAT_JSON_FILE}" ./privatekey.json
@@ -8,5 +11,8 @@
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/${MANAGED_SERVICE_ACCOUNTS_JSON_FILE}" ./
 ./google-cloud-sdk/bin/gsutil cp "gs://${GCLOUD_BUCKET_UAT}/${GOOGLE_ORG_WHITELIST_JSON_FILE}" ./
 
+cp *.json ./json
+cp *.txt ./txt
 
+echo "Beginning rsync of /static..."
 ./google-cloud-sdk/bin/gsutil rsync -R static/ gs://webapp-uat-static-files/static
