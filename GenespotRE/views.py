e45a03391747e94d1e617f1c80a46b81a8bbe74f
wlongabaugh <wlongabaugh@systemsbiology.org>
2018-06-27 15:06:19 -0700

    DebugDCFToo force

--- GenespotRE/views.py
+++ GenespotRE/views.py
@@ -136,7 +136,8 @@ def user_detail(request, user_id):
 
         user_details['gcp_list'] = len(GoogleProject.objects.filter(user=user))
 
-        nih_details = get_nih_user_details(user_id)
+        forced_logout = request.session['dcfForcedLogout'] if 'dcfForcedLogout' in request.session else None
+        nih_details = get_nih_user_details(user_id, forced_logout)
         for key in nih_details.keys():
             user_details[key] = nih_details[key]
 
