923323e6e47b935285846af41dbca36400460864
s-paquette <spaquett@systemsbiology.org>
2018-07-18 11:49:33 -0700

    -> Fix paths for CircleCI 2.0

--- shell/gcloud_authenticate.sh
+++ shell/gcloud_authenticate.sh
@@ -2,6 +2,6 @@ echo $TEST_PRIVATE_KEY_FOR_V2 | base64 --decode --ignore-garbage > deployment.ke
 
 echo "[STATUS] Generated JSON for $TEST_CLIENT_EMAIL, attempting to authenticate..."
 
-/home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file deployment.key.json
-/home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set account $TEST_CLIENT_EMAIL
-/home/ubuntu/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set project "$TEST_PROJECT_ID"
+/home/circleci/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file deployment.key.json
+/home/circleci/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set account $TEST_CLIENT_EMAIL
+/home/circleci/${CIRCLE_PROJECT_REPONAME}/google-cloud-sdk/bin/gcloud config set project "$TEST_PROJECT_ID"
