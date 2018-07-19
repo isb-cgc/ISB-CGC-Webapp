79439916187d27af1f622fd0094046d83cf35de7
s-paquette <spaquett@systemsbiology.org>
2018-07-18 17:09:25 -0700

    -> Fixing rsync destination

--- shell/unpack_for_deployment.sh
+++ shell/unpack_for_deployment.sh
@@ -11,7 +11,7 @@ STATIC_LAST_COMMIT=$(git rev-list -1 HEAD -- "static")
 
 if [ ! -f "${STATIC_COMMIT_CHECK_FILE}" ] || [ ${STATIC_LAST_COMMIT} != $(cat "${STATIC_COMMIT_CHECK_FILE}") ]; then
     echo "Beginning rsync of /static..."
-    ./google-cloud-sdk/bin/gsutil rsync -R static/ gs://${GCLOUD_STATIC_BUCKET_TEST}
+    ./google-cloud-sdk/bin/gsutil rsync -R static/ gs://${GCLOUD_STATIC_BUCKET_TEST}/static
     git rev-list -1 HEAD -- "static" > "${STATIC_COMMIT_CHECK_FILE}"
     ./google-cloud-sdk/bin/gsutil cp "${STATIC_COMMIT_CHECK_FILE}" gs://${GCLOUD_BUCKET_TEST}/
 else
