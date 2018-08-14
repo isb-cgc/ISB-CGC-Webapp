85c5b4c1b547dd4f1eb9665667621f8de931f4e8
s-paquette <spaquett@systemsbiology.org>
2018-08-13 18:13:30 -0700

    -> Fix for rsync check when there is no commit file

--- shell/unpack_for_deployment.sh
+++ shell/unpack_for_deployment.sh
@@ -9,7 +9,7 @@ ls ./*.json
 # Check if we need to rsync static
 STATIC_LAST_COMMIT=$(git rev-list -1 HEAD -- "static")
 
-if [[ -z "${STATIC_COMMIT_CHECK_FILE}"  || (-f "${STATIC_COMMIT_CHECK_FILE}" && ${STATIC_LAST_COMMIT} != $(cat "${STATIC_COMMIT_CHECK_FILE}")) ]]; then
+if [ ! -f "${STATIC_COMMIT_CHECK_FILE}" ] || [ ${STATIC_LAST_COMMIT} != $(cat "${STATIC_COMMIT_CHECK_FILE}") ]; then
     echo "Beginning rsync of /static..."
     ./google-cloud-sdk/bin/gsutil rsync -R static/ gs://webapp-uat-static-files/static
     git rev-list -1 HEAD -- "static" > ${STATIC_COMMIT_CHECK_FILE}
