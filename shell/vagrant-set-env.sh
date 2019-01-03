9f94448a3acca652aa2264ee3a5728dd5e0c8009
s-paquette <spaquett@systemsbiology.org>
2019-01-02 18:45:07 -0800

    -> Don't forget set-env...

--- shell/vagrant-set-env.sh
+++ shell/vagrant-set-env.sh
@@ -1,3 +1,3 @@
 echo 'export PYTHONPATH=/home/vagrant/www:/home/vagrant/www/lib:/home/vagrant/google_appengine' | tee -a /home/vagrant/.bash_profile
-echo 'export SECURE_LOCAL_PATH=secure_files/' | tee -a /home/vagrant/.bash_profile
+echo 'export SECURE_LOCAL_PATH=../parentDir/secure_files/' | tee -a /home/vagrant/.bash_profile
 chmod +x /home/vagrant/www/shell/python-su.sh
