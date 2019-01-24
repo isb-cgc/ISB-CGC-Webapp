06947f11a1b95eb9447bd0ecf5c2a45faaa73adf
eleeisb <elee@systemsbiology.org>
2019-01-24 09:36:43 -0800

    reversing table id change

--- bq_data_access/data_types/cnvr.py
+++ bq_data_access/data_types/cnvr.py
@@ -13,7 +13,7 @@ BIGQUERY_CONFIG = {
             "gencode_reference_table_id": "isb-cgc:genome_reference.GENCODE_v22",
             "genomic_build": "hg38",
             "value_field": "segment_mean",
-            "table_id": "isb-cgc:TCGA_hg38_data_v0.Copy_Number_Segment_Masked_r14",
+            "table_id": "isb-cgc:TCGA_hg38_data_v0.Copy_Number_Segment_Masked",
             "gene_label_field": "Hugo_Symbol",
             "internal_table_id": "cnvr_masked_hg38",
             "program": "tcga"
