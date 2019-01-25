59c5a79ea481251c1e50df09d3419ba21e744f7f
s-paquette <spaquett@systemsbiology.org>
2019-01-24 18:34:07 -0800

    -> No more using production tables, check your env for the project tables to use

--- bq_data_access/data_types/cnvr.py
+++ bq_data_access/data_types/cnvr.py
@@ -1,10 +1,12 @@
+from django.conf import settings
+
 BIGQUERY_CONFIG = {
     "tables": [
         {
             "gencode_reference_table_id": "isb-cgc:genome_reference.GENCODE_v19",
             "genomic_build": "hg19",
             "value_field": "segment_mean",
-            "table_id": "isb-cgc:TCGA_hg19_data_v0.Copy_Number_Segment_Masked",
+            "table_id": "{}:TCGA_hg19_data_v0.Copy_Number_Segment_Masked".format(settings.BIGQUERY_PROJECT_NAME),
             "gene_label_field": "Hugo_Symbol",
             "internal_table_id": "cnvr_masked_hg19",
             "program": "tcga"
@@ -13,7 +15,7 @@ BIGQUERY_CONFIG = {
             "gencode_reference_table_id": "isb-cgc:genome_reference.GENCODE_v22",
             "genomic_build": "hg38",
             "value_field": "segment_mean",
-            "table_id": "isb-cgc:TCGA_hg38_data_v0.Copy_Number_Segment_Masked",
+            "table_id": "{}:TCGA_hg38_data_v0.Copy_Number_Segment_Masked".format(settings.BIGQUERY_PROJECT_NAME),
             "gene_label_field": "Hugo_Symbol",
             "internal_table_id": "cnvr_masked_hg38",
             "program": "tcga"
--- bq_data_access/data_types/methylation.py
+++ bq_data_access/data_types/methylation.py
@@ -1,15 +1,17 @@
+from django.conf import settings
+
 BIGQUERY_CONFIG = {
     "methylation_annotation_table_id": "isb-cgc:platform_reference.methylation_annotation",
     "supported_genomic_builds": ['hg19', 'hg38'],
     "table_structure": [
         {
-            "table_id_prefix": "isb-cgc:TCGA_hg19_data_v0.DNA_Methylation_chr",
+            "table_id_prefix": "{}:TCGA_hg19_data_v0.DNA_Methylation_chr".format(settings.BIGQUERY_PROJECT_NAME),
             "genomic_build": "hg19",
             "value_field": "beta_value",
             "program": "tcga"
         },
         {
-            "table_id_prefix": "isb-cgc:TCGA_hg38_data_v0.DNA_Methylation_chr",
+            "table_id_prefix": "{}:TCGA_hg38_data_v0.DNA_Methylation_chr".format(settings.BIGQUERY_PROJECT_NAME),
             "genomic_build": "hg38",
             "value_field": "beta_value",
             "program": "tcga"
