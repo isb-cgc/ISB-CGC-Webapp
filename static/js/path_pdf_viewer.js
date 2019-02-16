833eeef26610c6cee2079fddd21fc33768744d2e
eleeisb <elee@systemsbiology.org>
2019-02-15 16:09:01 -0800

    full screen toggle feature added to camic, dicom, pdf viewers

--- static/js/path_pdf_viewer.js
+++ static/js/path_pdf_viewer.js
@@ -20,40 +20,77 @@ require.config({
     baseUrl: STATIC_FILES_URL+'js/',
     paths: {
         jquery: 'libs/jquery-1.11.1.min',
-        bootstrap: 'libs/bootstrap.min',
-        jqueryui: 'libs/jquery-ui.min',
-        session_security: 'session_security',
-        underscore: 'libs/underscore-min',
     },
     shim: {
-        'bootstrap': ['jquery'],
-        'jqueryui': ['jquery'],
-        'session_security': ['jquery'],
     }
 });
 
 require([
     'jquery',
-    'jqueryui',
-    'bootstrap',
-    'session_security'
 ], function ($) {
+
+    var fullscreen = false;
 
     $('#path-report-iframe').on('load',function(){
         $('.load-spinner').hide();
         // Resize the iFrame's height to center the view vertically
         if($('#path-pdf').length > 0) {
-            $('#path-report-iframe').height(($(window).height() - $('.navbar').height() - ($('#path-report-viewer').height() - $('#path-report-iframe').height())));
             $(window).scrollTop(0);
         }
+    });
+
+
+    var reset_spinner_dimension = function () {
+        $('.load-spinner').width(fullscreen ? '100vw' : $('.iframe-container iframe').css('width'));
+        $('.load-spinner').height(fullscreen ? '100vh': $('.iframe-container iframe').css('height'));
+    };
+
+    var openFullscreen = function() {
+        var iframe_container = document.querySelector('.iframe-container');
+        if (iframe_container.requestFullscreen) {
+            iframe_container.requestFullscreen();
+        } else if (iframe_container.mozRequestFullScreen) { /* Firefox */
+            iframe_container.mozRequestFullScreen();
+        } else if (iframe_container.webkitRequestFullscreen) { /* Chrome, Safari & Opera */
+            iframe_container.webkitRequestFullscreen();
+        } else if (iframe_container.msRequestFullscreen) { /* IE/Edge */
+            iframe_container.msRequestFullscreen();
+        }
+    };
+
+    var closeFullscreen = function() {
+        if (document.exitFullscreen) {
+            document.exitFullscreen();
+        } else if (document.mozCancelFullScreen) { /* Firefox */
+            document.mozCancelFullScreen();
+        } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+            document.webkitExitFullscreen();
+        } else if (document.msExitFullscreen) { /* IE/Edge */
+            document.msExitFullscreen();
+        }
+    };
+
+    $(document).bind('webkitfullscreenchange MSFullscreenChange mozfullscreenchange fullscreenchange', function(e) {
+        fullscreen = document.fullScreen || document.mozFullScreen || document.webkitIsFullScreen;
+        $('.iframe-container .toggle-fullscreen i').toggleClass('fa-compress', fullscreen);
+        $('.iframe-container .toggle-fullscreen i').toggleClass('fa-expand', !fullscreen);
+        $('.iframe-container').toggleClass('fullscreen', fullscreen);
+        reset_spinner_dimension();
+    });
+
+    $('.iframe-container .toggle-fullscreen').on('click',function() {
+        fullscreen ? closeFullscreen(): openFullscreen();
     });
 
     // Because we're operating a bit outside the Bootstrap framework on the path-pdf template,
     // we need to force the loading spinner to size properly
     if($('#path-pdf').length > 0) {
-        $('.load-spinner').width($('#path-report-iframe').css('width'));
-        $('.load-spinner').height($('#path-report-iframe').css('height'));
+        reset_spinner_dimension();
     }
 
     $('.load-spinner').show();
+
+    setTimeout(function(){
+        $('.load-spinner').hide();
+    },3500);
 });
