02969d9c6a05a636f3a7336d59ee937b1f80d42b
eleeisb <elee@systemsbiology.org>
2019-07-03 12:17:51 -0700

    bugfix

--- notebooks/migrations/0001_initial.py
+++ notebooks/migrations/0001_initial.py
@@ -6,9 +6,9 @@ from django.conf import settings
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('accounts', '0001_initial'),
     ]
 
     operations = [
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             name='Notebook',
             fields=[
                 ('id', models.AutoField(serialize=False, primary_key=True)),
-                ('name', models.CharField(max_length=2024)),
+                ('name', models.CharField(max_length=2024, null=False)),
                 ('file_path', models.CharField(max_length=1024, null=False)),
                 ('description', models.CharField(max_length=2024)),
                 ('keywords', models.CharField(max_length=2024)),
@@ -35,8 +35,6 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('notebook', models.ForeignKey(to='notebooks.Notebook')),
-                # ('perm', models.CharField(default=b'READER', max_length=10,
-                #                           choices=[(b'READER', b'Reader'), (b'OWNER', b'Owner')])),
                 ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL, blank=True)),
 
             ],
@@ -44,19 +42,19 @@ class Migration(migrations.Migration):
             },
             bases=(models.Model,),
         ),
-
-
-        # migrations.CreateModel(
-        #     name='Notebook_Last_View',
-        #     fields=[
-        #         ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-        #         ('notebook', models.ForeignKey(to='notebooks.Notebook')),
-        #         ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
-        #         ('test', models.DateTimeField(auto_now_add=True, null=True)),
-        #         ('last_view', models.DateTimeField(auto_now=True, auto_now_add=True)),
-        #     ],
-        #     options={
-        #     },
-        #     bases=(models.Model,),
-        # ),
+        migrations.CreateModel(
+            name='Instance',
+            fields=[
+                ('id', models.AutoField(serialize=False, primary_key=True)),
+                ('name', models.CharField(max_length=2024, null=False)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+                ('vm_username', models.CharField(max_length=20, null=False)),
+                ('gcp', models.ForeignKey(to='accounts.GoogleProject')),
+                ('zone', models.CharField(max_length=20, null=False)),
+                ('active', models.BooleanField(default=True))
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
     ]
