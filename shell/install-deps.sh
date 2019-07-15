72582dfeef4d0495d9c34e1f914561c6da19271b
s-paquette <spaquett@systemsbiology.org>
2019-07-15 11:06:40 -0700

    -> Roll back to 251 since rsync is still not fixed...

--- shell/install-deps.sh
+++ shell/install-deps.sh
@@ -75,7 +75,7 @@ if [ -z "${CI}" ] || [ ! -d "/usr/lib/google-cloud-sdk" ]; then
     echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
     apt-get install apt-transport-https ca-certificates
     curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
-    apt-get update && apt-get -y install google-cloud-sdk
+    apt-get update && apt-get -y install google-cloud-sdk=251.0
     apt-get -y install google-cloud-sdk-app-engine-python
     echo "Google Cloud SDK Installed"
 else
