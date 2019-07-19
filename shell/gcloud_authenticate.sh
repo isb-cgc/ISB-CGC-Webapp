47e902792a6ae55d69ea894d020df66b70bcf4e7
s-paquette <spaquett@systemsbiology.org>
2019-07-19 14:58:45 -0700

    -> Something amiss with the deployment key...

--- shell/gcloud_authenticate.sh
+++ shell/gcloud_authenticate.sh
@@ -1,5 +1,7 @@
 echo ${DEPLOYMENT_KEY} | base64 --decode --ignore-garbage > deployment.key.json
 
 gcloud auth activate-service-account --key-file deployment.key.json
+echo "Setting deployment client email to ${DEPLOYMENT_CLIENT_EMAIL}"
 gcloud config set account $DEPLOYMENT_CLIENT_EMAIL
+echo "Setting deployment project to ${DEPLOYMENT_PROJECT_ID}"
 gcloud config set project "$DEPLOYMENT_PROJECT_ID"
