a1b4d782adb94785361519e85d7d6a39de3f0a4e
s-paquette <spaquett@systemsbiology.org>
2019-08-08 16:53:05 -0700

    -> Sample test

--- workbooks/tests.py
+++ workbooks/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from workbooks.models import Workbook
 
 # Create your tests here.
+
+
+class WorkBooTestCase(TestCase):
+    def setUp(self):
+        Workbook.objects.create(name="TestWorkbook", description="A test workbook")
+
+    def test_workbook_test_is_running(self):
+        print("The workbook test ran!")
+        testwb = Workbook.object.get(name="TestWorkbook")
+        self.assertIsEqual(testwb.name, "TestWorkbook")
