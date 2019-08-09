e95d60a9f607551ccd964c405499b68cb8f8c68c
s-paquette <spaquett@systemsbiology.org>
2019-08-09 12:13:32 -0700

    -> Cohorts loading order issue?

--- workbooks/tests.py
+++ workbooks/tests.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import AnonymousUser, User
 
 class WorkBookTestCase(TestCase):
     def setUp(self):
-        self.test_wb_owner = User.objects.create_user(uername='test_user',email='test_user_email@isb-cgc.org', password='itsasecrettoeveryone')
+        self.test_wb_owner = User.objects.create_user(username='test_user', email='test_user_email@isb-cgc.org', password='itsasecrettoeveryone')
 
     def test_make_workbook(self):
         Workbook.objects.create(name="TestWorkbook", description="A test workbook", owner=self.test_wb_owner)
