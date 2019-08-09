400ceff58775a9f109b544da9035741dc7e9423e
s-paquette <spaquett@systemsbiology.org>
2019-08-09 12:04:43 -0700

    -> Try real actual test for workbook -> Fix bug in admin restrict (Django 1.10 upgrade)

--- workbooks/tests.py
+++ workbooks/tests.py
@@ -1,14 +1,15 @@
 from django.test import TestCase
 from workbooks.models import Workbook
+from django.contrib.auth.models import AnonymousUser, User
 
 # Create your tests here.
 
 
-class WorkBooTestCase(TestCase):
+class WorkBookTestCase(TestCase):
     def setUp(self):
-        Workbook.objects.create(name="TestWorkbook", description="A test workbook")
+        self.test_wb_owner = User.objects.create_user(uername='test_user',email='test_user_email@isb-cgc.org', password='itsasecrettoeveryone')
 
-    def test_workbook_test_is_running(self):
-        print("The workbook test ran!")
+    def test_make_workbook(self):
+        Workbook.objects.create(name="TestWorkbook", description="A test workbook", owner=self.test_wb_owner)
         testwb = Workbook.object.get(name="TestWorkbook")
         self.assertIsEqual(testwb.name, "TestWorkbook")
