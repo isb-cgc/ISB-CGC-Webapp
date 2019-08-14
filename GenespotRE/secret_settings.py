7706d49434573168149215a6af0239fc08b59b2c
s-paquette <spaquett@systemsbiology.org>
2019-08-13 18:59:59 -0700

    -> Sync up secret settings

--- GenespotRE/secret_settings.py
+++ GenespotRE/secret_settings.py
@@ -9,16 +9,33 @@ if os.environ.get('SECURE_LOCAL_PATH', None):
 dotenv.read_dotenv(join(dirname(__file__), env_path+'.env'))
 
 
-SETTINGS = {
-    'DATABASE': {
+database_config = {
+    'default': {
+        'ENGINE': os.environ.get('DATABASE_ENGINE', 'django.db.backends.mysql'),
+        'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('DATABASE_USER'),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD')
+    }
+}
+
+# On the build system, we need to use build-system specific database information
+
+if os.environ.get('CI', None) is not None:
+    database_config = {
         'default': {
             'ENGINE': os.environ.get('DATABASE_ENGINE', 'django.db.backends.mysql'),
-            'HOST': os.environ.get('DATABASE_HOST', '127.0.0.1'),
-            'PORT': os.environ.get('DATABASE_PORT', 3306),
-            'NAME': os.environ.get('DATABASE_NAME', 'dev'),
-            'USER': os.environ.get('DATABASE_USER'),
-            'PASSWORD': os.environ.get('DATABASE_PASSWORD')
+            'HOST': os.environ.get('DATABASE_HOST_BUILD', '127.0.0.1'),
+            'NAME': os.environ.get('DATABASE_NAME_BUILD', ''),
+            'USER': os.environ.get('DATABASE_USER_BUILD'),
+            'PASSWORD': os.environ.get('MYSQL_ROOT_PASSWORD_BUILD')
         }
+    }
+
+
+SETTINGS = {
+    'DATABASE': {
+        database_config
     },
 }
 
