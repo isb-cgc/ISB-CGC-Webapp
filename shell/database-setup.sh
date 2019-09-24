ab5bc5aca7d6b6ecefacad15f5eb1fd2f877fa4c
s-paquette <spaquett@systemsbiology.org>
2019-09-23 19:08:02 -0700

    -> ...typo...

--- shell/database-setup.sh
+++ shell/database-setup.sh
@@ -84,7 +84,7 @@ if [ ! -f ${HOMEROOT}/scripts/metadata_featdef_tables.sql ]; then
     sudo gsutil cp "gs://${GCLOUD_BUCKET_DEV_SQL}/dev_table_and_routines_file.sql" ${HOMEROOT}/scripts/metadata_featdef_tables.sql
 fi
 echo "Applying SQL Table File... (may take a while)"
-mysql -u$MYSQL_ROOT_USER -h $MYSQL_DB_HOST -p$MYSQL_ROOT_PASSWORD -D$DATABASE_NAME < ${HOMEROOT}/scripts/metadata_featdef_tables.sql.
+mysql -u$MYSQL_ROOT_USER -h $MYSQL_DB_HOST -p$MYSQL_ROOT_PASSWORD -D$DATABASE_NAME < ${HOMEROOT}/scripts/metadata_featdef_tables.sql
 
 echo "Adding Cohort/Site Data and bootstrapping Django project and program tables..."
 python3 ${HOMEROOT}/scripts/add_site_ids.py
