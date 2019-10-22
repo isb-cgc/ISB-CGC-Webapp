28e59476e1157c738cc9fe2289c898d2a7a5c8be
eleeisb <elee@systemsbiology.org>
2019-10-22 15:10:40 -0700

    -feature updates on bq_meatdata

--- static/js/bq_meta_search.js
+++ static/js/bq_meta_search.js
@@ -18,15 +18,18 @@
 
 require.config({
     paths: {
-        // 'jquery': 'libs/jquery-1.11.1.min',
+        'bootstrap': ['https://stackpath.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min', 'libs/bootstrap.min'],
         'jquery': ['//cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min', 'libs/jquery-1.11.1.min'],
         'dataTables': ['//cdn.datatables.net/1.10.18/js/dataTables.bootstrap.min'],
         'dataTables.net': ['//cdn.datatables.net/1.10.19/js/jquery.dataTables.min', 'libs/jquery.dataTables.min'],
-        'dataTables.rowGroup': ['//cdn.datatables.net/rowgroup/1.0.3/js/dataTables.rowGroup.min', 'libs/dataTables.rowGroup.min']
+        'dataTables.rowGroup': ['//cdn.datatables.net/rowgroup/1.0.3/js/dataTables.rowGroup.min', 'libs/dataTables.rowGroup.min'],
+        'chosen': 'https://cdnjs.cloudflare.com/ajax/libs/chosen/1.8.7/chosen.jquery.min'
     },
     shim: {
+        'bootstrap': ['jquery'],
         'dataTables.net': ['jquery'],
-        'dataTables.rowGroup': ['jquery', 'dataTables.net']
+        'dataTables.rowGroup': ['jquery', 'dataTables.net'],
+        'chosen':['jquery']
     },
     map: {
         '*': {
@@ -37,8 +40,10 @@ require.config({
 
 require([
     'jquery',
+    'bootstrap',
     'dataTables.net',
-    'dataTables.rowGroup'
+    'dataTables.rowGroup',
+    'chosen'
 ], function ($) {
     $(document).ready(function () {
         var table = $('#bqmeta').DataTable({
@@ -82,7 +87,14 @@ require([
                 },
                 {
                     'name': 'tableId',
-                    'data': 'tableReference.tableId'
+                    'data': 'tableReference.tableId',
+                    'render': function (data, type) {
+                        return type === 'display' ?
+                            '<div class="nowrap-ellipsis">' + data + '</div>' :
+                            data;
+                    },
+                    'width':'19%'
+                    // 'className': 'custom-width-150'
                 },
                 {
                     'name': 'fullId',
@@ -91,27 +103,74 @@ require([
                         return type === 'display' ?
                             '<div class="nowrap-ellipsis">' + data + '</div>' :
                             data;
-                    }
+                    },
+                    'width':'19%'
+                    // 'className': 'custom-width-150'
                 },
                 {
                     'name': 'status',
                     'data': function (data) {
-                        if(data.labels && data.labels.status){
-                            return data.labels.status;
+                        if(data.labels && data.labels.Status){
+                            return data.labels.Status;
                         }
                         else
                             return null;
+                    },
+                    'render': function(data, type){
+                        return (type === 'display' && data) ?
+                            data.replace().replace(/_/g,' ') : data;
                     }
                 },
                 {
                     'name': 'category',
                     'data': function (data) {
-                        if(data.labels && data.labels.category){
-                            return data.labels.category.replace('_',' ');
+                        if(data.labels && data.labels.Category){
+                            return data.labels.Category;
                         }
                         else
                             return null;
+                    },
+                    // 'render': format_label_display(data, type),
+                    'render': function(data, type){
+                        return format_label_display(data, type);
+                        // return (type === 'display' && data) ?
+                        //     data.replace(/_/g,' ') : data;
                     }
+                },
+                {
+                    'name': 'referenceGenome',
+                    'data': function (data) {
+                        return filtered_label_data(data.labels, 'Reference_Genome');
+
+                    },
+                    // 'render': function(data, type){
+                    //     return format_label_display(data, type);
+                    // },
+                    'visible': false
+                },
+                {
+                    'name': 'source',
+                    'data': function (data) {
+                        if(data.labels && data.labels.Source){
+                            return data.labels.Source;
+                        }
+                        else
+                            return null;
+                    },
+                    // 'render': function(data, type){
+                    //     return format_label_display(data, type);
+                    // },
+                    'visible': false
+                },
+                {
+                    'name': 'dataType',
+                    'data': function (data) {
+                        return filtered_label_data(data.labels, 'Data_Type');
+                    },
+                    // 'render': function(data, type) {
+                    //     return format_label_display(data, type);
+                    // },
+                    'visible': false
                 },
                 {
                     'name': 'numRows',
@@ -129,6 +188,19 @@ require([
                         return month + "/" + date.getDate() + "/" + date.getFullYear();
                     },
                     'searchable': false
+                },
+                {
+                    'name': 'preview',
+                    "className": 'tbl-preview',
+                    'data': function (row){
+                        return row.id.split(/[.:]/).join('/');
+                    },
+                    'render': function (data, type) {
+                        return type === 'display' ?
+                            '' : data;
+                    },
+                    'searchable': false,
+                    'orderable': false
                 },
                 {
                     'name': 'description',
@@ -172,6 +244,7 @@ require([
                     },
                     'visible': false
                 },
+
                 // {
                 //     'name': 'selfLink',
                 //     'data': 'selfLink',
@@ -185,12 +258,31 @@ require([
             // Only use this for a paginated api.
             serverSide: false,
             order: [[1, 'asc']],
+            // buttons: ['copy', 'excel', 'pdf']
             // rowGroup: {
             //     dataSrc: 'datasetId'
             // }
-            "drawCallback": function() {
+            drawCallback: function() {
+                // $('.tbl-preview').click(function (e) {
+                //     var tbl_path = $(this).attr('data-tbl-id');
+                //     // alert(BASE_URL+"/get_tbl_preview/"+tbl_path+"/");
+                //     $.ajax({
+                //         type: "GET",
+                //         url: BASE_URL+"/get_tbl_preview/"+tbl_path+"/",
+                //         // data: {
+                //         //     id: $(this).val(), // < note use of 'this' here
+                //         //     access_token: $("#access_token").val()
+                //         // },
+                //         success: function (data) {
+                //             alert('ok');
+                //         },
+                //         error: function (result) {
+                //             alert('error');
+                //         }
+                //     });
+                // });
                 // $('#status').val('current');
-
+                //
                 // $('i.hover-bubble')
                 //     .mouseenter(function () {
                 //     $(this).parent('td').find('div.hover-bubble').show();
@@ -220,7 +312,39 @@ require([
         $('.bq-select').on('change', function () {
             var column_name = $(this).attr('data-column-name');
             var select_vals = $(this).val();
+            // console.log(column_name +", "+select_vals);
             columnSearch(column_name, select_vals);
+        });
+
+        $('#bqmeta tbody').on('click', 'td.tbl-preview', function () {
+
+            var td = $(this).closest('td');
+            var tbl_path = table.cell(td).data();
+            var tr = $(this).closest('tr');
+            var row = table.row(tr);
+            if (row.child.isShown() && tr.hasClass('preview-shown')) {
+                // This row is already open - close it
+                row.child.hide();
+                tr.removeClass('shown preview-shown');
+            } else {
+                $.ajax({
+                    type: "GET",
+                    url: BASE_URL + "/get_tbl_preview/" + tbl_path + "/",
+                    // beforeSend: function() {
+                    //     tr.addClass('preview-loading');
+                    // },
+                    error: function (result) {
+                        alert('error');
+                    },
+                    success: function (data) {
+                        var schema_fields = row.data().schema.fields;
+                        row.child(format_tbl_preview(schema_fields, data.rows)).show();
+                        tr.removeClass('details-shown');
+                        // tr.removeClass('preview-loading');
+                        tr.addClass('shown preview-shown');
+                    }
+                });
+            }
         });
 
         var columnSearch = function(column_name, term, regex_search, smart_search) {
@@ -235,18 +359,25 @@ require([
         $('#bqmeta tbody').on('click', 'td.details-control', function () {
             var tr = $(this).closest('tr');
             var row = table.row(tr);
-            if (row.child.isShown()) {
+            if (row.child.isShown() && tr.hasClass('details-shown')) {
                 // This row is already open - close it
                 row.child.hide();
-                tr.removeClass('shown');
+                tr.removeClass('shown details-shown');
             }
             else {
                 // Open this row
                 row.child(format(row.data())).show();
-                tr.addClass('shown');
+                tr.addClass('shown details-shown');
+                tr.removeClass('preview-shown');
             }
         });
-        $('#status').val('current');
+        $('#bq-meta-form i.fa-info-circle').tooltip();
+        $(".autocomplete_select_box").chosen({
+            // disable_search_threshold: 10,
+            no_results_text: "Oops, nothing found!",
+            width: "100%"
+        });
+        $('#status').val('Current');
         $('#status').trigger('change');
 
     });
@@ -264,6 +395,24 @@ require([
             '<td><strong>Labels</strong></td>' +
             '<td>'+tokenize_labels(d.labels)+'</td>' +
             '</tr></table>';
+    }
+
+    function format_tbl_preview(schema_fields, rows){
+        var html_tbl = '<div style="overflow-x: scroll;max-width: 900px;"><table class="preview-table" style="width:90%;">';
+        html_tbl += '<tr>';
+        for(var f=0; f<schema_fields.length; f++){
+            html_tbl += '<th>' +schema_fields[f]['name'] + '</th>';
+        }
+        html_tbl += '</tr>';
+        for (var i=0; i<rows.length; i++){
+            html_tbl += '<tr>';
+            for(var j=0; j< rows[i]['f'].length; j++){
+                html_tbl += '<td nowrap>'+rows[i]['f'][j]['v']+'</td>';
+            }
+            html_tbl += '</tr>';
+        }
+        html_tbl += '</table></div>';
+        return html_tbl;
     }
 
     var tokenize_labels = function(labels_obj){
@@ -293,6 +442,20 @@ require([
         schema_table += '</table>';
         return schema_table;
     };
+
+    var filtered_label_data = function(data_labels, filter_key_term){
+        var filtered_val_arr = $.map(data_labels, function (val, key) {
+            return key.startsWith(filter_key_term) ? val : null;
+        });
+        return (filtered_val_arr.length > 0 ? filtered_val_arr.join(' ') : null);
+    };
+
+
+    var format_label_display = function(data, type){
+        return (type === 'display' && data) ?
+            data.replace(/_/g, ' ') : data;
+    };
+
 
 
 
