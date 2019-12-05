9ae577b02d2da87e326ab41dffeda8a6cde2bda9
S. Paquette <spaquett@systemsbiology.org>
2019-12-05 13:21:23 -0800

    -> No more cached Google deps -> Echo config

--- shell/gcloud_authenticate.sh
+++ shell/gcloud_authenticate.sh
@@ -2,11 +2,11 @@ if [ ! -f "deployment.key.json" ]; then
     echo ${DEPLOYMENT_KEY} | base64 --decode --ignore-garbage > deployment.key.json
 fi
 
-echo "Activating service account for deployment"
 gcloud auth activate-service-account --key-file deployment.key.json
-
-echo "Setting deployment account to ${DEPLOYMENT_CLIENT_EMAIL}"
 gcloud config set account $DEPLOYMENT_CLIENT_EMAIL
-
-echo "Setting deployment project to ${DEPLOYMENT_PROJECT_ID}"
 gcloud config set project "$DEPLOYMENT_PROJECT_ID"
+
+echo "GCloud Config for Deployment:"
+gcloud config
+
+
