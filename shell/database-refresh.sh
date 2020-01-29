e4f867b66d1a493dc29a7a1efa480d49023362f6
s-paquette <spaquett@systemsbiology.org>
2020-01-28 21:17:51 -0800

    -> Adding system of checks and failures for lack of env file found, to aid in debugging devops and testing

--- shell/database-refresh.sh
+++ shell/database-refresh.sh
@@ -8,7 +8,11 @@ else
         echo "[ERROR] Host seen: ${DATABASE_HOST}"
         exit 1
     fi
-    export $(cat ${ENV_FILE_PATH} | grep -v ^# | xargs) 2> /dev/null
+    if ( "/home/vagrant/www/shell/get_env.sh" ) ; then
+        export $(cat ${ENV_FILE_PATH} | grep -v ^# | xargs) 2> /dev/null
+    else
+        exit 1
+    fi
     export MYSQL_ROOT_USER=root
     echo "Dropping database $DATABASE_NAME in prepatation for reload..."
     mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "DROP DATABASE IF EXISTS $DATABASE_NAME"
