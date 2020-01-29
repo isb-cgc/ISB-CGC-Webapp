1130ca834cac1548a2ed72f6060c0bf1d4495a2b
s-paquette <spaquett@systemsbiology.org>
2020-01-28 21:16:01 -0800

    -> Add in a failsafe for possible accidental refresh of a remote database

--- shell/database-refresh.sh
+++ shell/database-refresh.sh
@@ -1,6 +1,13 @@
 if [ -n "$CI" ]; then
-    export "[ERROR] Never refresh databases on deployments!"
+    echo "[ERROR] Never refresh databases on deployments!"
+    exit 1
 else
+    # Doublecheck where we're running this...
+    if [ $DATABASE_HOST != "localhost" ]; then
+        echo "[ERROR] Possible remote database detected! This script IS ONLY intended for use on local developer builds!"
+        echo "[ERROR] Host seen: ${DATABASE_HOST}"
+        exit 1
+    fi
     export $(cat ${ENV_FILE_PATH} | grep -v ^# | xargs) 2> /dev/null
     export MYSQL_ROOT_USER=root
     echo "Dropping database $DATABASE_NAME in prepatation for reload..."
