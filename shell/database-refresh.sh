f86ba1bb6e375ec005b78a81e2d6d54a87071543
S. Paquette <spaquett@systemsbiology.org>
2020-04-07 20:29:00 -0700

    -> Allow creation of superuser on CircleCI test database

--- shell/database-refresh.sh
+++ shell/database-refresh.sh
@@ -4,7 +4,13 @@ if [ -n "$CI" ]; then
 else
     if ( "/home/vagrant/www/shell/get_env.sh" ) ; then
         export $(cat ${ENV_FILE_PATH} | grep -v ^# | xargs) 2> /dev/null
+        if [ -z "${SECURE_LOCAL_PATH}" ] || [ "${SECURE_LOCAL_PATH}" == "" ] ; then
+            echo "[ERROR] SECURE_LOCAL_PATH not found, but this is a VM build! Something might be wrong with your .env file"
+            echo "or your secure_files directory."
+            exit 1
+        fi
     else
+        echo "[ERROR] Unable to run get_env.sh!"
         exit 1
     fi
     # Doublecheck where we're running this...
@@ -13,7 +19,6 @@ else
         echo "[ERROR] Host seen: ${DATABASE_HOST}"
         exit 1
     fi
-    export MYSQL_ROOT_USER=root
     echo "Dropping database $DATABASE_NAME in prepatation for reload..."
     mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "DROP DATABASE IF EXISTS $DATABASE_NAME"
     mysql -u$MYSQL_ROOT_USER -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE $DATABASE_NAME"
