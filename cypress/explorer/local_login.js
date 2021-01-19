35c7c6f3bf8bf350e74ae497739dcd09d5b7052a
George <george@butter.local>
2021-01-19 09:18:30 -0800

    cypress changes

--- cypress/explorer/local_login.js
+++ cypress/explorer/local_login.js
@@ -1,3 +1,4 @@
+
 import { login } from '../util/utils.js'
 
 
@@ -6,21 +7,7 @@ describe('Visit explore', () => {
 
   it('Goes to explore', () => {
 
-   
      login()
-
-     /* cy.server();
-     cy.route('Get', 'http://localhost:8085/').as('explore');
-     cy.visit('http://localhost:8085/') ;
-     cy.get('button').filter('.btn-warn').click();
-     cy.viewport(1300,800);
-     cy.get('#sign-in-dropdown').click();
-     cy.get('#id_login').type(Cypress.env("idcuser"));
-     cy.get('#id_password').type(Cypress.env("idcpassword"));
-     cy.get('.primaryAction').click();
-     cy.wait(1000);
-     */
-     // cy.visit('http://localhost:8085/explore/')
 
  }) 
 
