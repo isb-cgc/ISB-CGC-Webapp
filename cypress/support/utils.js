3c138316ee9a40c0883466cd064d7a8d67523d46
George <george@butter.local>
2021-04-28 08:38:45 -0700

    update cypress for google login

--- cypress/support/utils.js
+++ cypress/support/utils.js
@@ -1,27 +1,29 @@
 export const localLogin=() =>{
-
- /*    cy.get('button').filter('.btn-warn').its('length').then(res=>
-       {
-         if(res>0){
-             cy.get('button').filter('.btn-warn').click({force:true})
-         }
-     
-      }
-     ); */
-     cy.viewport(1300,800);
-     cy.get('#sign-in-dropdown').click();
-     cy.get('#id_login').type(Cypress.env("idcuser"));
-     cy.get('#id_password').type(Cypress.env("idcpassword"));
-     cy.get('.primaryAction').click();
-     cy.wait(1000);
+   cy.viewport(1300,800);
+   cy.get('#sign-in-dropdown').click();
+   cy.get('#id_login').type(Cypress.env("idcuser"));
+   cy.get('#id_password').type(Cypress.env("idcpassword"));
+   cy.get('.primaryAction').click();
+   cy.wait(1000);
 
 }
 
-export const visitPage= function(url,login) {
-
+export const visitPage= function(url,loginReqested) {
    cy.server();
-   cy.fixture('login_reqs').as('reqs')
-   
+   var loggedIn = false;
+   var mustLogIn = false;
+   var nurl= Cypress.config().baseUrl+url.split('?')[0];
+
+   if ( (cy.fixture('login_reqs').hasOwnProperty(nurl) && cy.fixture('login_reqs')[nurl]) || (Cypress.env('alwaysLogin')==="true") || loginRequested ){
+       var mustLogin= true;
+   }
+
+   if (mustLogin && (Cypress.env('loginType')==="google")){
+      cy.setCookie("sessionid",Cypress.env('sessionid'));
+      cy.setCookie("csrftoken",Cypress.env('csrftoken'));
+      loggedIn=true;
+   }
+
    cy.visit(url).then( function() {
       cy.wait(1000);
       cy.get('button').filter('.btn-warn').its('length').then(res=>
@@ -29,18 +31,10 @@ export const visitPage= function(url,login) {
           if(res>0){
            cy.get('button').filter('.btn-warn').click({force:true})
           }
-
          cy.viewport(1300,800);
-
-         var nurl= Cypress.config().baseUrl+url.split('?')[0];
-         cy.log(nurl);
-         cy.log(this.reqs[nurl]);
-         if (this.reqs.hasOwnProperty(nurl) && this.reqs[nurl]){
-             if (Cypress.env('loginType')==='local'){
-              localLogin();
-          }
-       }
-   
+         if (mustLogin && !(loggedIn) && (Cypress.env('loginType')==='local') ){
+             localLogin();
+          } 
     });
 
  });  
