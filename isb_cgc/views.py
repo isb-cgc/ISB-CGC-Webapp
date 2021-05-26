4502f9d17c162483750484d50ef0957abee59dde
mitianisb <mtian@systemsbiology.org>
2021-05-25 23:21:39 -0700

    Shut off email notification  from UAT

--- isb_cgc/views.py
+++ isb_cgc/views.py
@@ -929,8 +929,12 @@ def opt_in_form_submitted(request):
                     "submitted_time": datetime.datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
                 }
                 BigQueryFeedbackSupport.add_rows_to_table([feedback_row])
-                # send a notification to feedback@isb-cgc.org about the entry
-                send_feedback_notification(feedback_row)
+
+                if settings.IS_UAT:
+                    logger.info("[STATUS] UAT: sent email for feedback")
+                else:
+                    # send a notification to feedback@isb-cgc.org about the entry
+                    send_feedback_notification(feedback_row)
                 msg = 'We thank you for your time and suggestions.'
         else:
             error_msg = 'We were not able to find a user with the given email. Please check with us again later.'
