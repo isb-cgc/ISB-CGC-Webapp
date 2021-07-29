4c18a575f342e0b529fc2eea3f2e6050e522e1f6
s-paquette <spaquett@systemsbiology.org>
2021-07-29 15:04:03 -0700

    -> No convention checking for now

--- shell/run_lint.sh
+++ shell/run_lint.sh
@@ -14,10 +14,10 @@ shopt -s globstar
 
 echo "::::::::::::::::::::: Running Pylint on ISB-CGC-WebApp modules :::::::::::::::::::::"
 
-python3 -m pylint adminrestrict analysis bq_data_access genes isb_cgc offline scripts seqpeek session_security variables visualizations workbooks ./*.py
+python3 -m pylint -d C adminrestrict analysis bq_data_access genes isb_cgc offline scripts seqpeek session_security variables visualizations workbooks ./*.py
 
 echo "::::::::::::::::::::: Running Pylint on ISB-CGC-Common modules :::::::::::::::::::::"
 
-python3 -m pylint accounts cohorts solr_helpers sharing projects metadata_utils dataset_utils
+python3 -m pylint -d C accounts cohorts solr_helpers sharing projects metadata_utils dataset_utils
 
 exit 0
