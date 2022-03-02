7be67b4d0dfa632846f626c1467f22954fb28d81
S. Paquette <spaquett@systemsbiology.org>
2022-03-01 17:36:48 -0800

    -> Local builds on Xenial from here on out

--- Vagrantfile
+++ Vagrantfile
@@ -1,8 +1,8 @@
 # -*- mode: ruby -*-
 # vi: set ft=ruby :
 Vagrant.configure(2) do |config|
-  config.vm.box_url = "https://app.vagrantup.com/ubuntu/boxes/xenial64"
-  config.vm.box = "ubuntu/xenial64"
+  config.vm.box_url = "https://app.vagrantup.com/ubuntu/boxes/bionic64"
+  config.vm.box = "ubuntu/bionic64"
 
   # WebApp ports
   config.vm.network "forwarded_port", guest: 8085, host: 8085
--- shell/get_env.sh
+++ shell/get_env.sh
@@ -1,5 +1,5 @@
 if [ -z "${ENV_FILE_PATH}" ] || [ ! -f "${ENV_FILE_PATH}" ]; then
-    echo "Environment variables file wasn't found - doublecheck secure_files.env and make sure it is a valid, VM-relative path!"
+    echo "Environment variables file wasn't found - doublecheck secure_path.env and make sure it is a valid, VM-relative path!"
     echo "Current value of ENV_FILE_PATH is: ${ENV_FILE_PATH}"
     exit 1
 fi
