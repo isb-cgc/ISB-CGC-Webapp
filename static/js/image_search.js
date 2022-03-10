dc2a2657267ff61cfcfad8698d4817673a541470
S. Paquette <spaquett@systemsbiology.org>
2022-03-10 11:25:09 -0800

    -> ImagingDataCommons/IDC-WebApp#730

--- static/js/image_search.js
+++ static/js/image_search.js
@@ -59,7 +59,7 @@ require([
     window.projSets['tcga']=["tcga_blca", "tcga_brca", "tcga_cesc", "tcga_coad", "tcga_esca", "tcga_gbm", "tcga_hnsc", "tcga_kich", "tcga_kirc", "tcga_kirp", "tcga_lgg", "tcga_lihc", "tcga_luad", "tcga_lusc", "tcga_ov", "tcga_prad", "tcga_read", "tcga_sarc", "tcga_stad", "tcga_thca", "tcga_ucec"];
     window.projSets['rider']=["rider_lung_ct", "rider_phantom_pet_ct","rider_breast_mri", "rider_neuro_mri","rider_phantom_mri", "rider_lung_pet_ct"];
     window.projSets['qin'] = ["qin_headneck","qin_lung_ct","qin_pet_phantom","qin_breast_dce_mri"];
-
+    var first_filter_load = true;
 
 
     var plotLayout = {
@@ -1695,6 +1695,13 @@ require([
 
     var updateFacetsData = function (newFilt) {
         update_filter_url();
+        if(window.location.href.search(/\/filters\//g) >= 0) {
+            if(!first_filter_load) {
+                window.history.pushState({}, '', BASE_URL + "/explore/")
+            } else {
+                first_filter_load = false;
+            }
+        }
         var url = '/explore/'
         var parsedFiltObj=parseFilterObj();
         url= encodeURI('/explore/')
