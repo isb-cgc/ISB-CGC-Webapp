3a5b481725c1048cf6368ad9d1356752a0e03470
S. Paquette <spaquett@systemsbiology.org>
2022-04-25 08:05:28 -0700

    -> Circle CI image updates -> YAMLs should be avoided in gitignore -> Copyright date updates

--- idc/checkreqsize_middleware.py
+++ idc/checkreqsize_middleware.py
@@ -1,5 +1,5 @@
 ###
-# Copyright 2015-2019, Institute for Systems Biology
+# Copyright 2015-2022, Institute for Systems Biology
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
