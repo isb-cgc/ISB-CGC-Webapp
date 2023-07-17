02ac16be719c3792ff46fcc6c5c63ef7fa65548e
S. Paquette <spaquett@systemsbiology.org>
2023-07-17 15:06:23 -0700

    -> Creating expr tier

--- shell/pull_config.sh
+++ shell/pull_config.sh
@@ -1,7 +1,7 @@
-if [ ! -f "/home/circleci/${CIRCLE_PROJECT_REPONAME}/deployment_config.txt" ]; then
-    gsutil cp gs://${DEPLOYMENT_BUCKET}/deployment_config.txt /home/circleci/${CIRCLE_PROJECT_REPONAME}/
-    chmod ugo+r /home/circleci/${CIRCLE_PROJECT_REPONAME}/deployment_config.txt
-    if [ ! -f "/home/circleci/${CIRCLE_PROJECT_REPONAME}/deployment_config.txt" ]; then
+if [ ! -f "/home/circleci/${CIRCLE_PROJECT_REPONAME}/${DEPLOYMENT_CONFIG}" ]; then
+    gsutil cp gs://${DEPLOYMENT_BUCKET}/${DEPLOYMENT_CONFIG} /home/circleci/${CIRCLE_PROJECT_REPONAME}/
+    chmod ugo+r /home/circleci/${CIRCLE_PROJECT_REPONAME}/${DEPLOYMENT_CONFIG}
+    if [ ! -f "/home/circleci/${CIRCLE_PROJECT_REPONAME}/${DEPLOYMENT_CONFIG}" ]; then
       echo "[ERROR] Couldn't assign deployment configuration file - exiting."
       exit 1
     fi
