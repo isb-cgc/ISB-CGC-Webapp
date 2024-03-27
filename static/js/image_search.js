d7526e9df6c3f3f0d1abd469bf45ebc32690126a
S. Paquette <spaquett@systemsbiology.org>
2024-03-27 10:48:23 -0700

    -> Per discussion disable all SR individual viewing for now (pending #1365 to pick the correct one case by case)

--- static/js/image_search.js
+++ static/js/image_search.js
@@ -1441,7 +1441,7 @@ require([
     }
 
     window.updateSeriesTable = function(rowsAdded, rowsRemoved, refreshAfterFilter,seriesID) {
-        var nonViewAbleModality= new Set(["PR","SEG","RTSTRUCT","RTPLAN","RWV", "XC"])
+        var nonViewAbleModality= new Set(["PR","SEG","RTSTRUCT","RTPLAN","RWV", "XC", "SR"])
         var nonViewAbleSOPClassUID= new Set(["1.2.840.10008.5.1.4.1.1.66"])
         var slimViewAbleModality=new Set(["SM"])
         $('#series_tab').attr('data-rowsremoved', rowsRemoved);
