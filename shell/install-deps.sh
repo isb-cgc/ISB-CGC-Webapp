16056e0944f0232c552693ee6f0716a6b7afb39c
s-paquette <spaquett@systemsbiology.org>
2024-03-29 11:45:43 -0700

    -> Force rebuild via updates

--- shell/install-deps.sh
+++ shell/install-deps.sh
@@ -45,8 +45,8 @@ echo "Preparing System..."
 apt-get -y --force-yes install software-properties-common ca-certificates
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 467B942D3A79BD29
-wget "https://repo.mysql.com/mysql-apt-config_0.8.26-1_all.deb" -P /tmp
-dpkg --install /tmp/mysql-apt-config_0.8.26-1_all.deb
+wget "https://repo.mysql.com/mysql-apt-config_0.8.29-1_all.deb" -P /tmp
+dpkg --install /tmp/mysql-apt-config_0.8.29-1_all.deb
 
 apt-get update -qq
 
