d4df344ff911f9c781fc753a6094d961c99d3e50
S. Paquette <spaquett@systemsbiology.org>
2024-04-02 09:16:54 -0700

    -> jquery update

--- static/js/sapien.js
+++ static/js/sapien.js
@@ -19,7 +19,7 @@
 require.config({
     baseUrl: STATIC_FILES_URL + 'js/',
     paths: {
-        jquery: 'libs/jquery-3.5.1',
+        jquery: 'libs/jquery-3.7.1.min',
         jqueryui: 'libs/jquery-ui.min',
         underscore: 'libs/underscore-min',
         base: 'base',
