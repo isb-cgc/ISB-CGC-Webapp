0445e89124c6bed5c27b84c575572bbef0b8dec8
s-paquette <spaquett@systemsbiology.org>
2024-06-06 12:35:29 -0700

    -> Datetime fix

--- isb_cgc/views.py
+++ isb_cgc/views.py
@@ -1,15 +1,17 @@
-"""
-Copyright 2015-2019, Institute for Systems Biology
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-   http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
+###
+#
+# Copyright 2015-2024, Institute for Systems Biology
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#    http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###
 
 from builtins import str
 from builtins import map
@@ -117,7 +119,7 @@ def _decode_dict(data):
 @never_cache
 def landing_page(request):
     logger.info("[STATUS] Received landing page view request at {}".format(
-        datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
+        datetime.now().strftime("%Y-%m-%d %H:%M:%S")))
     return render(request, 'isb_cgc/landing.html',
                   {'bq_search_url': settings.BQ_SEARCH_URL, 'mitelman_url': settings.MITELMAN_URL,
                    'tp53_url': settings.TP53_URL})
