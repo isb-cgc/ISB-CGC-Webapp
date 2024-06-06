e4c888c087b69c09ef4dd4f6c13c356ca7ad8a37
s-paquette <spaquett@systemsbiology.org>
2024-06-06 15:17:51 -0700

    -> Add token template

--- isb_cgc/settings.py
+++ isb_cgc/settings.py
@@ -515,7 +515,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 OTP_EMAIL_SENDER = os.environ.get('OTP_EMAIL_SENDER', SUPPORT_EMAIL)
 OTP_EMAIL_SUBJECT = os.environ.get('OTP_EMAIL_SUBJECT', "[ISB-CGC] Email Login Token")
-OTP_EMAIL_BODY_TEMPLATE_PATH = os.environ.get('OTP_EMAIL_BODY_TEMPLATE_PATH', 'isb_cgc/token.txt')
+OTP_EMAIL_BODY_TEMPLATE_PATH = os.environ.get('OTP_EMAIL_BODY_TEMPLATE_PATH', 'isb_cgc/token.html')
 OTP_LOGIN_URL = os.environ.get('OTP_LOGIN_URL', '/otp_request/')
 
 #########################################
