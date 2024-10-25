6912051db0968bbeebd58f7a36b4bc80657cf13f
S. Paquette <spaquett@systemsbiology.org>
2024-10-25 14:10:23 -0700

    -> No using SASS anymore, so remove it

--- shell/vagrant-start-server.sh
+++ shell/vagrant-start-server.sh
@@ -1,7 +1,2 @@
 export $(cat ${ENV_FILE_PATH} | grep -v ^# | xargs) 2> /dev/null
 
-echo "Killing any running processes..."
-sudo killall -9 sass 2> /dev/null
-sleep 10
-echo "Starting SASS compiler..."
-sass --poll --watch /home/vagrant/www/sass/main.sass:/home/vagrant/www/static/css/main.css > /home/vagrant/parentDir/outputSASS.log 2> /home/vagrant/parentDir/errorSASS.log &
