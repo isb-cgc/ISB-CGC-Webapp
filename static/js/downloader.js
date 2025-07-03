86bb4db0665b8614d92b7112b7844fabacf78544
S. Paquette <spaquett@systemsbiology.org>
2025-07-03 16:27:31 -0700

    -> Move worker code to its own file

--- static/js/downloader.js
+++ static/js/downloader.js
@@ -33,87 +33,6 @@ require([
     let downloadWorker = null;
     const s3_urls = [];
 
-    const workerCode = `
-      async function createNestedDirectories(topLevelDirectoryHandle, path) {
-        const pathSegments = path.split('/').filter((segment) => segment !== '')
-        let currentDirectoryHandle = topLevelDirectoryHandle
-        for (const segment of pathSegments) {
-          try {
-            // Attempt to get the directory handle without creating it
-            const entry = await currentDirectoryHandle.getDirectoryHandle(segment, {
-              create: false,
-            })
-            currentDirectoryHandle = entry
-          } catch (error) {
-            // If the error is specifically about the directory not existing, create it
-            if (error .name === 'NotFoundError') {
-              const entry = await currentDirectoryHandle.getDirectoryHandle(segment, {
-                create: true,
-              })
-              currentDirectoryHandle = entry
-            } else {
-              // Handle other potential errors (e.g., name conflicts)
-              return false // Indicate failure
-            }
-          }
-        }
-        // Return the last directory handle
-        return currentDirectoryHandle
-      }
-
-      importScripts('https://cdn.jsdelivr.net/npm/dcmjs@0.41.0/build/dcmjs.min.js')
-
-      function dicomValue(dataset,tagName) {
-          let value = "Undefined-"+tagName
-          const entry = dcmjs.data.DicomMetaDictionary.nameMap[tagName]
-          if (entry && entry.tag) {
-            const hexTag = entry.tag.replace("(","").replace(",","").replace(")","")
-            if (hexTag in dataset.dict) {
-              value = dataset.dict[hexTag].Value
-            }
-          }
-          return value
-      }
-
-      self.onmessage = async function(event) {
-          const s3_url = event.data.url
-          try {
-            const directoryHandle = event.data.directoryHandle
-            const collection_id = event.data.collection_id || 'unknown_collection'
-            response = await fetch(s3_url)
-            if (!response.ok) {
-                console.error('Worker: Failed to fetch URL:', s3_url, response.statusText)
-                self.postMessage({message: "error", error: "Failed to fetch URL"})
-                return
-            }
-            const arrayBuffer = await response.arrayBuffer()
-            const dataset = dcmjs.data.DicomMessage.readFile(arrayBuffer)
-            const modality = dicomValue(dataset,"Modality")
-            const patientID = dicomValue(dataset,"PatientID")
-            const sopInstanceUID = dicomValue(dataset,"SOPInstanceUID")
-            const studyInstanceUID = dicomValue(dataset,"StudyInstanceUID")
-            const seriesInstanceUID = dicomValue(dataset,"SeriesInstanceUID")
-            const seriesDirectory = modality + "_" + seriesInstanceUID
-            const filePath = [collection_id,patientID,studyInstanceUID,seriesDirectory].join("/")
-            const fileName = sopInstanceUID + ".dcm"
-            const blob = new Blob([arrayBuffer], { type: 'application/dicom' })
-            const file = new File([blob], fileName, { type: 'application/dicom' })
-            const subDirectoryHandle = await createNestedDirectories(directoryHandle, filePath)
-            const fileHandle = await subDirectoryHandle.getFileHandle(fileName, { create: true, })
-            const writable = await fileHandle.createWritable()
-            await writable.write(arrayBuffer)
-            await writable.close()
-            self.postMessage({message: "done", path: s3_url, localFilePath: filePath})
-          } catch (error) {
-            console.error("Error when attempting to fetch URL "+s3_url);
-            console.error(error);
-            self.postMessage({message: "error", path: s3_url, error: error})
-          }
-      }
-    `;
-
-    const workerCodeBlob = new Blob([workerCode], { type: 'application/javascript' });
-    const workerObjectURL = URL.createObjectURL(workerCodeBlob);
     const workerDownloadThreshold = 100;
     const availableWorkers = [];
 
@@ -147,17 +66,17 @@ require([
     }
 
     function allocateWorker() {
-      downloadWorker = new Worker(workerObjectURL);
+      downloadWorker = new Worker(STATIC_FILES_URL + 'js/downloadWorker.js');
       downloadWorker.onmessage = workerOnMessage;
       downloadWorker.onerror = function(event) {
-        let thisWorker = event.target
+        let thisWorker = event.target;
         console.error('Main: Error in worker:', event.message || "No message given", event);
         statusMessage(`Error in worker: ${event.message}`, 'error', true);
         finalizeWorker(thisWorker);
       }
       downloadWorkers.downloadCount = 0;
       downloadWorkers.push(downloadWorker);
-      return downloadWorker
+      return downloadWorker;
     }
 
     let workerLimit = navigator.hardwareConcurrency;
@@ -165,7 +84,6 @@ require([
 
     function triggerWorkerDownloads() {
       if (s3_urls.length == 0 && downloadWorkers.length == 0) {
-        if (workerObjectURL) URL.revokeObjectURL(workerObjectURL);
         statusMessage(`Downloads complete`, 'info', true);
       } else {
         while (s3_urls.length > 0) {
