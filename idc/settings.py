e9d22486bd94aa643c41f6052c589f9af1df9aca
S. Paquette <spaquett@systemsbiology.org>
2025-09-02 10:52:17 -0700

    -> Report the application version seen in settings.py

--- idc/settings.py
+++ idc/settings.py
@@ -160,7 +160,7 @@ else:
             print(e)
             VERSION = "{}.{}.{}".format("local-dev", datetime.datetime.now().strftime('%Y%m%d%H%M'), "unavailable")
     else:
-        print("[ERROR] Application version file not found for test/prod deployment! Exiting...")
+        print("[ERROR] Application version file not found for non-dev deployment! Exiting...")
         exit(1)
 
 APP_VERSION = os.environ.get("APP_VERSION", VERSION)
@@ -168,6 +168,7 @@ APP_VERSION = os.environ.get("APP_VERSION", VERSION)
 DEV_TIER = bool(DEBUG or re.search(r'^local-dev\.', APP_VERSION))
 
 print("[STATUS] DEV_TIER setting is {}".format(DEV_TIER))
+print("[STATUS] Application version is {}".format(APP_VERSION))
 
 # If this is a GAE-Flex deployment, we don't need to specify SSL; the proxy will take
 # care of that for us
